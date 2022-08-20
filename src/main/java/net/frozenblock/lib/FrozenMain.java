5f8c4e79248b8138329252046f491477ea47c4c8
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-20 13:28:43 -0400

    City Test

--- src/main/java/net/frozenblock/lib/FrozenMain.java
+++ src/main/java/net/frozenblock/lib/FrozenMain.java
@@ -3,6 +3,7 @@ package net.frozenblock.lib;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.frozenblock.lib.replacements_and_lists.BlockScheduledTicks;
+import net.frozenblock.lib.replacements_and_lists.StructurePoolElementIdReplacements;
 import net.frozenblock.lib.sound.RegisterMovingSoundRestrictions;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
@@ -22,6 +23,9 @@ public final class FrozenMain implements ModInitializer {
         RegisterMovingSoundRestrictions.init();
         if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
             BlockScheduledTicks.ticks.put(Blocks.DIAMOND_BLOCK, (state, world, pos, random) -> world.setBlock(pos, Blocks.BEDROCK.defaultBlockState(), 3));
+            StructurePoolElementIdReplacements.resourceLocationReplacements.put(new ResourceLocation("ancient_city/city_center/city_center_1"), id("ancient_city/city_center/city_center_2"));
+            StructurePoolElementIdReplacements.resourceLocationReplacements.put(new ResourceLocation("ancient_city/city_center/city_center_2"), id("ancient_city/city_center/city_center_2"));
+            StructurePoolElementIdReplacements.resourceLocationReplacements.put(new ResourceLocation("ancient_city/city_center/city_center_3"), id("ancient_city/city_center/city_center_2"));
         }
 
     }
