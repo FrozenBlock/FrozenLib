bd20d13b3aa6fcf60122bdd3d30af3ec4e213c82
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-21 15:50:13 -0400

    CHANGE COOLDOWN

---
+++ src/main/java/net/frozenblock/lib/mixin/server/ServerItemCooldownsMixin.java
@@ -0,0 +1,37 @@
+package net.frozenblock.lib.mixin.server;
+
+import io.netty.buffer.Unpooled;
+import net.fabricmc.fabric.impl.networking.server.ServerNetworkingImpl;
+import net.frozenblock.lib.FrozenMain;
+import net.frozenblock.lib.interfaces.CooldownInterface;
+import net.minecraft.core.Registry;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemCooldowns;
+import net.minecraft.world.item.ServerItemCooldowns;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(ServerItemCooldowns.class)
+public class ServerItemCooldownsMixin extends ItemCooldowns implements CooldownInterface {
+    @Shadow @Final
+    private ServerPlayer player;
+
+    public void changeCooldown(Item item, int additional) {
+        if (this.cooldowns.containsKey(item)) {
+            CooldownInstance cooldown = this.cooldowns.get(item);
+            this.cooldowns.put(item, new CooldownInstance(cooldown.startTime, cooldown.endTime + additional));
+            this.onCooldownChanged(item, additional);
+        }
+    }
+
+    public void onCooldownChanged(Item item, int additional) {
+        FriendlyByteBuf byteBuf = new FriendlyByteBuf(Unpooled.buffer());
+        byteBuf.writeId(Registry.ITEM, item);
+        byteBuf.writeVarInt(additional);
+        this.player.connection.send(ServerNetworkingImpl.createPlayC2SPacket(FrozenMain.COOLDOWN_CHANGE_PACKET, byteBuf));
+    }
+
+}
