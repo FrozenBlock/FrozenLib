a30fb183c8a39bd97bc11c24b8ad23efca92f6bc
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-22 19:59:41 -0400

    Revert "testing"
    This reverts commit 86fcfd4f21998ebd877f93293a0b40b198fa3ea8.

--- src/main/java/net/frozenblock/lib/mixin/worldgen/SinglePoolElementMixin.java
+++ src/main/java/net/frozenblock/lib/mixin/worldgen/SinglePoolElementMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.world.level.levelgen.structure.pools.SinglePoolElement;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorList;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
-import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -16,7 +15,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SinglePoolElement.class)
 public abstract class SinglePoolElementMixin {
@@ -24,23 +22,12 @@ public abstract class SinglePoolElementMixin {
     @Shadow @Final @Mutable
     public Either<ResourceLocation, StructureTemplate> template;
 
-    /*@Inject(method = "<init>", at = @At("TAIL"))
+    @Inject(method = "<init>", at = @At("TAIL"))
     public void init(Either<ResourceLocation, StructureTemplate> template, Holder<StructureProcessorList> processors, StructureTemplatePool.Projection projection, CallbackInfo info) {
         if (template.left().isPresent()) {
             ResourceLocation id = template.left().get();
             if (StructurePoolElementIdReplacements.resourceLocationReplacements.containsKey(id)) {
                 this.template = Either.left(StructurePoolElementIdReplacements.resourceLocationReplacements.get(id));
-            }
-        }
-    }*/
-
-    @Inject(method = "getTemplate", at = @At("HEAD"), cancellable = true)
-    private void getTemplate(StructureTemplateManager structureTemplateManager, CallbackInfoReturnable<StructureTemplate> info) {
-        if (template.left().isPresent()) {
-            ResourceLocation id = template.left().get();
-            if (StructurePoolElementIdReplacements.resourceLocationReplacements.containsKey(id)) {
-                info.cancel();
-                info.setReturnValue(structureTemplateManager.getOrCreate(StructurePoolElementIdReplacements.resourceLocationReplacements.get(id)));
             }
         }
     }
