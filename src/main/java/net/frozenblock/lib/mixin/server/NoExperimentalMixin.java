125c2549036f79ac7aadff3531e8d2a66285e48e
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-22 20:07:47 -0400

    Alt anti-experimental mixin

--- src/main/java/net/frozenblock/lib/mixin/server/NoExperimentalMixin.java
+++ src/main/java/net/frozenblock/lib/mixin/server/NoExperimentalMixin.java
@@ -1,19 +1,45 @@
 package net.frozenblock.lib.mixin.server;
 
+import com.mojang.datafixers.DataFixer;
 import com.mojang.serialization.Lifecycle;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.LevelSettings;
+import net.minecraft.world.level.border.WorldBorder;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.levelgen.WorldGenSettings;
 import net.minecraft.world.level.storage.PrimaryLevelData;
+import net.minecraft.world.level.timers.TimerQueue;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Mixin(PrimaryLevelData.class)
 public class NoExperimentalMixin {
 
-    @Inject(method = "worldGenSettingsLifecycle", at = @At("HEAD"), cancellable = true)
+    @Shadow
+    @Final
+    @Mutable
+    private Lifecycle worldGenSettingsLifecycle;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void init(@Nullable DataFixer dataFixer, int i, @Nullable CompoundTag compoundTag, boolean bl, int j, int k, int l, float f, long m, long n, int o, int p, int q, boolean bl2, int r, boolean bl3, boolean bl4, boolean bl5, WorldBorder.Settings settings, int s, int t, @Nullable UUID uUID, Set<String> set, TimerQueue<MinecraftServer> timerQueue, @Nullable CompoundTag compoundTag2, CompoundTag compoundTag3, LevelSettings levelSettings, WorldGenSettings worldGenSettings, Lifecycle lifecycle, CallbackInfo info) {
+        this.worldGenSettingsLifecycle = Lifecycle.stable();
+    }
+
+    /*@Inject(method = "worldGenSettingsLifecycle", at = @At("HEAD"), cancellable = true)
     public void worldGenSettingsLifecycle(CallbackInfoReturnable<Lifecycle> info) {
         info.cancel();
         info.setReturnValue(Lifecycle.stable());
-    }
+    }*/
 
 }
