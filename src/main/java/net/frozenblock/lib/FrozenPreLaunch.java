4b7b710bbc1999cfb7f8c87fe60640a3cc94eff5
Treetrain1 <treetrain1@gmail.com>
2022-09-02 16:25:03 -0500

    dont you just LOVE RENDERING ISSUES

--- src/main/java/net/frozenblock/lib/FrozenPreLaunch.java
+++ src/main/java/net/frozenblock/lib/FrozenPreLaunch.java
@@ -6,8 +6,8 @@ import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 public class FrozenPreLaunch implements PreLaunchEntrypoint {
     @Override
     public void onPreLaunch() {
-        if (FabricLoader.getInstance().isDevelopmentEnvironment()) if (FabricLoader.getInstance().getModContainer("wilderwild").isEmpty()) {
+        /*if (FabricLoader.getInstance().isDevelopmentEnvironment()) if (FabricLoader.getInstance().getModContainer("wilderwild").isEmpty()) {
             System.exit(69420);
-        }
+        }*/
     }
 }
