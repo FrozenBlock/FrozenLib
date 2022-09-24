585e23158ff1789d9721ff3cf6e419948126f73e
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-24 13:22:47 -0400

    Except Selector

--- src/main/java/net/frozenblock/lib/worldgen/biome/api/modifications/FrozenBiomeSelectors.java
+++ src/main/java/net/frozenblock/lib/worldgen/biome/api/modifications/FrozenBiomeSelectors.java
@@ -3,9 +3,13 @@ package net.frozenblock.lib.worldgen.biome.api.modifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectionContext;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.biome.v1.NetherBiomes;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.dimension.LevelStem;
 
+import java.util.Arrays;
 import java.util.function.Predicate;
 
 /**
@@ -42,5 +46,60 @@ public final class FrozenBiomeSelectors {
      */
     public static Predicate<BiomeSelectionContext> foundInTheEnd() {
         return context -> context.hasTag(BiomeTags.IS_END) || context.canGenerateIn(LevelStem.END);
+    }
+
+    /**
+     * Returns a biome selector that will match all biomes that would normally spawn in the Overworld,
+     * assuming Vanilla's default biome source is used, and without specific biomes that are listed.
+     */
+    @SafeVarargs
+    public static Predicate<BiomeSelectionContext> foundInOverworldExcept(ResourceKey<Biome>... except) {
+        return context -> (context.hasTag(BiomeTags.IS_OVERWORLD) || context.canGenerateIn(LevelStem.OVERWORLD)) && !Arrays.stream(except).toList().contains(context.getBiomeKey());
+    }
+
+    /**
+     * Returns a biome selector that will match all biomes that would normally spawn in the Nether,
+     * assuming Vanilla's default multi noise biome source with the nether preset is used, and without specific biomes that are listed.
+     *
+     * <p>This selector will also match modded biomes that have been added to the nether using {@link NetherBiomes}.
+     */
+    @SafeVarargs
+    public static Predicate<BiomeSelectionContext> foundInTheNetherExcept(ResourceKey<Biome>... except) {
+        return context -> (context.hasTag(BiomeTags.IS_NETHER) || context.canGenerateIn(LevelStem.NETHER)) && !Arrays.stream(except).toList().contains(context.getBiomeKey());
+    }
+
+    /**
+     * Returns a biome selector that will match all biomes that would normally spawn in the End,
+     * assuming Vanilla's default End biome source is used, and without specific biomes that are listed.
+     */
+    @SafeVarargs
+    public static Predicate<BiomeSelectionContext> foundInTheEndExcept(ResourceKey<Biome>... except) {
+        return context -> (context.hasTag(BiomeTags.IS_END) || context.canGenerateIn(LevelStem.END)) && !Arrays.stream(except).toList().contains(context.getBiomeKey());
+    }
+
+    /**
+     * Returns a biome selector that will match all biomes that would normally spawn in the Overworld,
+     * assuming Vanilla's default biome source is used, except the specified biome.
+     */
+    public static Predicate<BiomeSelectionContext> foundInOverworldExcept(ResourceKey<Biome> except) {
+        return context -> (context.hasTag(BiomeTags.IS_OVERWORLD) || context.canGenerateIn(LevelStem.OVERWORLD)) && except != context.getBiomeKey();
+    }
+
+    /**
+     * Returns a biome selector that will match all biomes that would normally spawn in the Nether,
+     * assuming Vanilla's default multi noise biome source with the nether preset is used, except the specified biome.
+     *
+     * <p>This selector will also match modded biomes that have been added to the nether using {@link NetherBiomes}.
+     */
+    public static Predicate<BiomeSelectionContext> foundInTheNetherExcept(ResourceKey<Biome> except) {
+        return context -> (context.hasTag(BiomeTags.IS_NETHER) || context.canGenerateIn(LevelStem.NETHER)) && except != context.getBiomeKey();
+    }
+
+    /**
+     * Returns a biome selector that will match all biomes that would normally spawn in the End,
+     * assuming Vanilla's default End biome source is used, except the specified biome.
+     */
+    public static Predicate<BiomeSelectionContext> foundInTheEndExcept(ResourceKey<Biome> except) {
+        return context -> (context.hasTag(BiomeTags.IS_END) || context.canGenerateIn(LevelStem.END)) && except != context.getBiomeKey();
     }
 }
