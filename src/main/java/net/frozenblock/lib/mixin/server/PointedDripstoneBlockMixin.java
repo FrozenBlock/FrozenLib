6c091b660febcc142debd034cbe3630b6a43f1c3
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-24 14:58:18 -0400

    Update PointedDripstoneBlockMixin.java

--- src/main/java/net/frozenblock/lib/mixin/server/PointedDripstoneBlockMixin.java
+++ src/main/java/net/frozenblock/lib/mixin/server/PointedDripstoneBlockMixin.java
@@ -40,7 +40,7 @@ public class PointedDripstoneBlockMixin {
     private static VoxelShape REQUIRED_SPACE_TO_DRIP_THROUGH_NON_SOLID_BLOCK;
 
     @SuppressWarnings("UnresolvedMixinReference")
-    @Inject(method = "m_ulptarvl(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/PointedDripstoneBlock$FluidInfo;", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"), remap = false, locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
+    @Inject(method = "m_ulptarvl(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/PointedDripstoneBlock$FluidInfo;", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"), remap = true, locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
     private static void getFluidAboveStalactite(Level level, BlockPos pos, CallbackInfoReturnable<PointedDripstoneBlock.FluidInfo> cir, BlockPos blockPos, BlockState blockState) {
         if (blockPos != null) {
             if (DripstoneDripWaterFrom.map.containsKey(blockState.getBlock()) && !level.dimensionType().ultraWarm()) {
