b9b18260f1639d74255d95ad144c0e84a5a1b868
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-24 11:27:24 -0400

    Move To FrozenLib

--- src/main/java/net/frozenblock/lib/mixin/server/PointedDripstoneBlockMixin.java
+++ src/main/java/net/frozenblock/lib/mixin/server/PointedDripstoneBlockMixin.java
@@ -1,16 +1,22 @@
 package net.frozenblock.lib.mixin.server;
 
+import net.frozenblock.lib.replacements_and_lists.DripstoneDripLavaFrom;
+import net.frozenblock.lib.replacements_and_lists.DripstoneDripWaterFrom;
 import net.frozenblock.lib.tags.FrozenBlockTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.AbstractCauldronBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.PointedDripstoneBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -19,7 +25,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Optional;
 import java.util.function.BiPredicate;
@@ -31,6 +39,41 @@ public class PointedDripstoneBlockMixin {
     @Final
     @Shadow
     private static final VoxelShape REQUIRED_SPACE_TO_DRIP_THROUGH_NON_SOLID_BLOCK = Block.box(6.0, 0.0, 6.0, 10.0, 16.0, 10.0);
+
+    private static BlockPos savedBlockPos;
+
+    @Inject(method = "m_ulptarvl(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/PointedDripstoneBlock$FluidInfo;", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private static void getFluidAboveStalactite(Level level, BlockPos pos, CallbackInfoReturnable<PointedDripstoneBlock.FluidInfo> cir, BlockPos blockPos, BlockState blockState) {
+        if (DripstoneDripWaterFrom.map.containsKey(blockState.getBlock()) && !level.dimensionType().ultraWarm() && savedBlockPos != null) {
+            cir.setReturnValue(new PointedDripstoneBlock.FluidInfo(savedBlockPos, Fluids.WATER, blockState));
+            savedBlockPos = null;
+        }
+        if (DripstoneDripLavaFrom.map.containsKey(blockState.getBlock()) && savedBlockPos != null) {
+            cir.setReturnValue(new PointedDripstoneBlock.FluidInfo(savedBlockPos, Fluids.LAVA, blockState));
+            savedBlockPos = null;
+        }
+    }
+
+    @Inject(method = "m_ulptarvl(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/PointedDripstoneBlock$FluidInfo;", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/core/BlockPos;above()Lnet/minecraft/core/BlockPos;"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private static void getFluidAboveStalactite1(Level level, BlockPos pos, CallbackInfoReturnable<PointedDripstoneBlock.FluidInfo> cir, BlockPos blockPos) {
+        savedBlockPos = blockPos;
+    }
+
+
+    @Inject(method = "maybeTransferFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private static void maybeTransferFluid(BlockState state, ServerLevel level, BlockPos pos, float randChance, CallbackInfo ci, Optional<PointedDripstoneBlock.FluidInfo> optional, Fluid fluid, float f, BlockPos blockPos) {
+        if (optional.isPresent()) {
+            PointedDripstoneBlock.FluidInfo fluidInfo = optional.get();
+            Block block = optional.get().sourceState().getBlock();
+            if (DripstoneDripWaterFrom.map.containsKey(block) && fluid == Fluids.WATER) {
+                DripstoneDripWaterFrom.map.get(block).drip(level, fluidInfo, blockPos);
+                ci.cancel();
+            } else if (DripstoneDripLavaFrom.map.containsKey(block) && fluid == Fluids.LAVA) {
+                DripstoneDripLavaFrom.map.get(block).drip(level, fluidInfo, blockPos);
+                ci.cancel();
+            }
+        }
+    }
 
     @Inject(at = @At("HEAD"), method = "findFillableCauldronBelowStalactiteTip", cancellable = true)
     private static void findFillableCauldronBelowStalactiteTip(Level world, BlockPos pos2, Fluid fluid, CallbackInfoReturnable<BlockPos> info) {
@@ -72,6 +115,26 @@ public class PointedDripstoneBlockMixin {
             if (!world.isOutsideBuildHeight(mutable.getY()) && continuePredicate.test(mutable, blockState)) continue;
             return Optional.empty();
         }
+        return Optional.empty();
+    }
+
+    @Shadow
+    private static boolean isStalactite(BlockState state) {
+        return isPointedDripstoneWithDirection(state, Direction.DOWN);
+    }
+
+    @Shadow
+    private static boolean isStalagmite(BlockState state) {
+        return isPointedDripstoneWithDirection(state, Direction.UP);
+    }
+
+    @Shadow
+    private static boolean isPointedDripstoneWithDirection(BlockState state, Direction dir) {
+        return state.is(Blocks.POINTED_DRIPSTONE) && state.getValue(BlockStateProperties.VERTICAL_DIRECTION) == dir;
+    }
+
+    @Shadow
+    private static Optional<BlockPos> findRootBlock(Level level, BlockPos pos, BlockState state, int maxIterations) {
         return Optional.empty();
     }
 
---
+++ src/main/java/net/frozenblock/lib/replacements_and_lists/DripstoneDripLavaFrom.java
@@ -0,0 +1,20 @@
+package net.frozenblock.lib.replacements_and_lists;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.PointedDripstoneBlock;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DripstoneDripLavaFrom {
+
+    public static Map<Block, InjectedOnDrip> map = new HashMap<>();
+
+    @FunctionalInterface
+    public interface InjectedOnDrip {
+        void drip(ServerLevel world, PointedDripstoneBlock.FluidInfo fluidInfo, BlockPos pos);
+    }
+
+}
