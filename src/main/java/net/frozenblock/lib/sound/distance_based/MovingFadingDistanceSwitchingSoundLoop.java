b4da5621d5cba0e21f02fa70c3886a9a7d387388
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-10-11 17:07:55 -0400

    Lunade casually using their brain

---
+++ src/main/java/net/frozenblock/lib/sound/distance_based/MovingFadingDistanceSwitchingSoundLoop.java
@@ -0,0 +1,79 @@
+package net.frozenblock.lib.sound.distance_based;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.frozenblock.lib.sound.FrozenSoundPredicates;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.AbstractTickableSoundInstance;
+import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
+
+@Environment(EnvType.CLIENT)
+public class MovingFadingDistanceSwitchingSoundLoop extends AbstractTickableSoundInstance {
+
+    private final Entity entity;
+    private final FrozenSoundPredicates.LoopPredicate<?> predicate;
+    private final boolean isFarSound;
+    private final double maxDist;
+    private final double fadeDist;
+    private final float maxVol;
+
+    public MovingFadingDistanceSwitchingSoundLoop(Entity entity, SoundEvent sound, SoundSource category, float volume, float pitch, FrozenSoundPredicates.LoopPredicate<?> predicate, boolean isFarSound, double maxDist, double fadeDist, float maxVol) {
+        super(sound, category, SoundInstance.createUnseededRandom());
+        this.entity = entity;
+        this.looping = true;
+        this.delay = 0;
+        this.volume = volume;
+        this.pitch = pitch;
+
+        this.x = (float) entity.getX();
+        this.y = (float) entity.getY();
+        this.z = (float) entity.getZ();
+        this.predicate = predicate;
+        this.isFarSound = isFarSound;
+        this.maxDist = maxDist;
+        this.fadeDist = fadeDist;
+        this.maxVol = maxVol;
+    }
+
+    @Override
+    public boolean canPlaySound() {
+        return !this.entity.isSilent();
+    }
+
+    @Override
+    public boolean canStartSilent() {
+        return true;
+    }
+
+    @Override
+    public void tick() {
+        Minecraft client = Minecraft.getInstance();
+        if (this.entity.isRemoved()) {
+            this.stop();
+        } else {
+            if (!this.predicate.test(this.entity)) {
+                this.stop();
+            } else {
+                this.x = (float) this.entity.getX();
+                this.y = (float) this.entity.getY();
+                this.z = (float) this.entity.getZ();
+                if (client.player != null) {
+                    float distance = client.player.distanceTo(this.entity);
+                    if (distance < this.fadeDist) {
+                        this.volume = !this.isFarSound ? this.maxVol : 0.001F;
+                    } else if (distance > this.maxDist) {
+                        this.volume = this.isFarSound ? this.maxVol : 0.001F;
+                    } else {
+                        //Gets lower as you move farther
+                        float fadeProgress = (float) ((this.maxDist - distance) / (this.maxDist - this.fadeDist));
+                        this.volume = this.isFarSound ? (1F - fadeProgress) * this.maxVol : fadeProgress * this.maxVol;
+                    }
+                }
+            }
+        }
+    }
+
+}
