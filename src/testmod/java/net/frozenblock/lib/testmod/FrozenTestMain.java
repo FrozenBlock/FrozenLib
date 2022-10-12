5706751ebbca04f89a686542a3645bc574e3d8c6
Treetrain1 <treetrain1@gmail.com>
2022-10-11 19:59:35 -0500

    FrozenLib Test Mod

---
+++ src/testmod/java/net/frozenblock/lib/testmod/FrozenTestMain.java
@@ -0,0 +1,10 @@
+package net.frozenblock.lib.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class FrozenTestMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
