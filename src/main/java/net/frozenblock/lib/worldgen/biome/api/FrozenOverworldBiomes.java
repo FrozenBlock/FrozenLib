c233f9f0fbcd1d207f69cfb46d8fc394bcfecde3
Treetrain1 <treetrain1@gmail.com>
2022-10-20 16:58:58 -0500

    Add some biome tools

--- src/main/java/net/frozenblock/lib/worldgen/biome/api/FrozenOverworldBiomes.java
+++ src/main/java/net/frozenblock/lib/worldgen/biome/api/FrozenOverworldBiomes.java
@@ -1,9 +1,13 @@
 package net.frozenblock.lib.worldgen.biome.api;
 
+import net.frozenblock.lib.worldgen.biome.api.parameters.FrozenBiomeParameters;
 import net.frozenblock.lib.worldgen.biome.impl.OverworldBiomeData;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Climate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * API that exposes the internals of Minecraft's overworld biome code.
@@ -42,6 +46,91 @@ public final class FrozenOverworldBiomes {
     public static void addOverworldBiome(ResourceKey<Biome> biome, Climate.ParameterPoint parameterPoint) {
         OverworldBiomeData.addOverworldBiome(biome, parameterPoint);
     }
+
+	/**
+	 * Adds a biome to the Overworld generator.
+	 *
+	 * @param biome			The {@link Biome} to add. Must not be null.
+	 * @param weirdnesses	The specific weirdnesses the biome should be added to.
+	 */
+	public static void addOverworldBiome(
+			ResourceKey<Biome> biome,
+			Climate.Parameter temperature,
+			Climate.Parameter humidity,
+			Climate.Parameter continentalness,
+			Climate.Parameter erosion,
+			float offset,
+			Climate.Parameter... weirdnesses
+	) {
+		addOverworldBiome(biome, temperature, humidity, continentalness, erosion, offset, List.of(weirdnesses));
+	}
+
+	/**
+	 * Adds a biome to the Overworld generator.
+	 *
+	 * @param biome			The {@link Biome} to add. Must not be null.
+	 * @param weirdnesses	The specific weirdnesses the biome should be added to.
+	 */
+	public static void addOverworldBiome(
+			ResourceKey<Biome> biome,
+			Climate.Parameter temperature,
+			Climate.Parameter humidity,
+			Climate.Parameter continentalness,
+			Climate.Parameter erosion,
+			float offset,
+			List<Climate.Parameter> weirdnesses
+	) {
+		addOverworldBiome(biome, temperature, humidity, continentalness, Climate.Parameter.point(0.0F), erosion, offset, weirdnesses);
+		addOverworldBiome(biome, temperature, humidity, continentalness, Climate.Parameter.point(1.0F), erosion, offset, weirdnesses);
+	}
+
+	/**
+	 * Adds a biome to the Overworld generator.
+	 *
+	 * @param biome			The {@link Biome} to add. Must not be null.
+	 * @param weirdnesses	The specific weirdnesses the biome should be added to.
+	 */
+	public static void addOverworldBiome(
+			ResourceKey<Biome> biome,
+			Climate.Parameter temperature,
+			Climate.Parameter humidity,
+			Climate.Parameter continentalness,
+			Climate.Parameter erosion,
+			Climate.Parameter depth,
+			float offset,
+			Climate.Parameter... weirdnesses
+	) {
+		addOverworldBiome(biome, temperature, humidity, continentalness, erosion, depth, offset, List.of(weirdnesses));
+	}
+
+	/**
+	 * Adds a biome to the Overworld generator.
+	 *
+	 * @param biome			The {@link Biome} to add. Must not be null.
+	 * @param weirdnesses	The specific weirdnesses the biome should be added to.
+	 */
+	public static void addOverworldBiome(
+			ResourceKey<Biome> biome,
+			Climate.Parameter temperature,
+			Climate.Parameter humidity,
+			Climate.Parameter continentalness,
+			Climate.Parameter erosion,
+			Climate.Parameter depth,
+			float offset,
+			List<Climate.Parameter> weirdnesses
+	) {
+		FrozenBiomeParameters.addWeirdness(weirdness -> OverworldBiomeData.addOverworldBiome(
+				biome, Climate.parameters(
+						temperature,
+						humidity,
+						continentalness,
+						erosion,
+						depth,
+						weirdness,
+						offset
+				)
+		), weirdnesses);
+	}
 
 	/**
 	 * Returns true if the given biome can generate in the Overworld, considering the Vanilla Overworld biomes,
---
+++ src/main/java/net/frozenblock/lib/worldgen/biome/api/parameters/FrozenBiomeParameters.java
@@ -0,0 +1,21 @@
+package net.frozenblock.lib.worldgen.biome.api.parameters;
+
+import net.minecraft.world.level.biome.Climate;
+import java.util.List;
+
+public final class FrozenBiomeParameters {
+	private FrozenBiomeParameters() {
+		throw new UnsupportedOperationException("FrozenBiomeParameters only contains static declarations.");
+	}
+
+	public static void addWeirdness(BiomeRunnable runnable, List<Climate.Parameter> weirdnesses) {
+		for (Climate.Parameter weirdness : weirdnesses) {
+			runnable.run(weirdness);
+		}
+	}
+
+	@FunctionalInterface
+	public interface BiomeRunnable {
+		void run(Climate.Parameter weirdness);
+	}
+}
