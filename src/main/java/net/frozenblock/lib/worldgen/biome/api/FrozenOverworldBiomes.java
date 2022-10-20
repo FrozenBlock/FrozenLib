75ae3f1d82c2de4f6867c7b78e6c5f87c21455cf
Treetrain1 <treetrain1@gmail.com>
2022-10-20 16:09:29 -0500

    add some javadocs

--- src/main/java/net/frozenblock/lib/worldgen/biome/api/FrozenOverworldBiomes.java
+++ src/main/java/net/frozenblock/lib/worldgen/biome/api/FrozenOverworldBiomes.java
@@ -5,10 +5,21 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Climate;
 
+/**
+ * API that exposes the internals of Minecraft's overworld biome code.
+ */
 public final class FrozenOverworldBiomes {
     private FrozenOverworldBiomes() {
+		throw new UnsupportedOperationException("FrozenOverworldBiomes only contains static declarations.");
     }
 
+	/**
+	 * Adds a biome to the Overworld generator.
+	 *
+	 * @param biome			The biome to add. Must not be null.
+	 * @param targetPoint	data about the given {@link Biome}'s spawning information in the Overworld.
+	 * @see Climate.TargetPoint
+	 */
     public static void addOverworldBiome(ResourceKey<Biome> biome, Climate.TargetPoint targetPoint) {
         OverworldBiomeData.addOverworldBiome(biome, Climate.parameters(
                 targetPoint.temperature(),
@@ -21,10 +32,21 @@ public final class FrozenOverworldBiomes {
         ));
     }
 
+	/**
+	 * Adds a biome to the Overworld generator.
+	 *
+	 * @param biome				The {@link Biome} to add. Must not be null.
+	 * @param parameterPoint	data about the given {@link Biome}'s spawning information in the Overworld.
+	 * @see Climate.ParameterPoint
+	 */
     public static void addOverworldBiome(ResourceKey<Biome> biome, Climate.ParameterPoint parameterPoint) {
         OverworldBiomeData.addOverworldBiome(biome, parameterPoint);
     }
 
+	/**
+	 * Returns true if the given biome can generate in the Overworld, considering the Vanilla Overworld biomes,
+	 * and any biomes added to the Overworld by mods.
+	 */
     public static boolean canGenerateInOverworld(ResourceKey<Biome> biome) {
         return OverworldBiomeData.canGenerateInOverworld(biome);
     }
