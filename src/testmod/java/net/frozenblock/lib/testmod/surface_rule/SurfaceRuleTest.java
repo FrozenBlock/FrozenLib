72e7fea6545d5ceef8a8d367ec12dac8936f0ae7
Treetrain1 <treetrain1@gmail.com>
2022-10-30 13:52:47 -0500

    quilt license headers

--- src/testmod/java/net/frozenblock/lib/testmod/surface_rule/SurfaceRuleTest.java
+++ src/testmod/java/net/frozenblock/lib/testmod/surface_rule/SurfaceRuleTest.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright 2022 FrozenBlock
+ * This file is part of FrozenLib.
+ *
+ * FrozenLib is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * FrozenLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with FrozenLib. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.frozenblock.lib.testmod.surface_rule;
 
 import org.jetbrains.annotations.NotNull;
