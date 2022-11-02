36805eaf66ff298917d479e899f40adf2f9547d1
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-02 19:18:28 -0400

    Update MovingParticleSoundLoop.java

--- src/main/java/net/frozenblock/lib/sound/MovingParticleSoundLoop.java
+++ src/main/java/net/frozenblock/lib/sound/MovingParticleSoundLoop.java
@@ -22,40 +22,50 @@ import net.minecraft.sounds.SoundSource;
 @Environment(EnvType.CLIENT)
 public class MovingParticleSoundLoop<T extends Particle> extends AbstractTickableSoundInstance {
 
-    private final T particle;
+	private final T particle;
+	private final int fadeInTicks;
+	private final float increaseVolumeBy;
+	private int ticks;
 
-    public MovingParticleSoundLoop(T particle, SoundEvent sound, SoundSource category, float volume, float pitch) {
-        super(sound, category, SoundInstance.createUnseededRandom());
-        this.particle = particle;
-        this.looping = true;
-        this.delay = 0;
-        this.volume = volume;
-        this.pitch = pitch;
+	public MovingParticleSoundLoop(T particle, SoundEvent sound, SoundSource category, float volume, float pitch, int fadeInTicks) {
+		super(sound, category, SoundInstance.createUnseededRandom());
+		this.particle = particle;
+		this.looping = true;
+		this.delay = 0;
+		this.pitch = pitch;
 
-        this.x = (float) particle.x;
-        this.y = (float) particle.y;
-        this.z = (float) particle.z;
-    }
+		this.x = (float) particle.x;
+		this.y = (float) particle.y;
+		this.z = (float) particle.z;
 
-    @Override
-    public boolean canPlaySound() {
-        return this.particle.isAlive();
-    }
+		this.fadeInTicks = fadeInTicks;
+		this.increaseVolumeBy = fadeInTicks != 0 ? (volume / fadeInTicks) : volume;
+		this.volume = fadeInTicks != 0 ? 0 : volume;
+	}
 
-    @Override
-    public boolean canStartSilent() {
-        return true;
-    }
+	@Override
+	public boolean canPlaySound() {
+		return this.particle.isAlive();
+	}
 
-    @Override
-    public void tick() {
-        if (this.particle == null || !this.particle.isAlive()) {
-            this.stop();
-        } else {
+	@Override
+	public boolean canStartSilent() {
+		return true;
+	}
+
+	@Override
+	public void tick() {
+		if (this.ticks < this.fadeInTicks) {
+			this.volume += this.increaseVolumeBy;
+			this.ticks += 1;
+		}
+		if (this.particle == null || !this.particle.isAlive()) {
+			this.stop();
+		} else {
 			this.x = (float) this.particle.x;
 			this.y = (float) this.particle.y;
 			this.z = (float) this.particle.z;
-        }
-    }
+		}
+	}
 
 }
