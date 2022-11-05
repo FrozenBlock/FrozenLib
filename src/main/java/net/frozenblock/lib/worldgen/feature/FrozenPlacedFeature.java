e070805dc37389db40e695eba0734c27a1ff8445
Treetrain1 <treetrain1@gmail.com>
2022-11-04 20:14:18 -0500

    placed

--- src/main/java/net/frozenblock/lib/worldgen/feature/FrozenPlacedFeature.java
+++ src/main/java/net/frozenblock/lib/worldgen/feature/FrozenPlacedFeature.java
@@ -1,5 +1,6 @@
 package net.frozenblock.lib.worldgen.feature;
 
+import net.frozenblock.lib.worldgen.feature.util.FrozenConfiguredFeatureUtils;
 import net.frozenblock.lib.worldgen.feature.util.FrozenPlacementUtils;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceKey;
@@ -20,16 +21,22 @@ public class FrozenPlacedFeature {
 	private final ResourceKey<PlacedFeature> resourceKey;
 	private final ResourceKey<ConfiguredFeature<?, ?>> featureKey;
 	private final List<PlacementModifier> placementModifiers;
+	private final PlacedFeature placedFeature;
 
 
 	public FrozenPlacedFeature(ResourceKey<PlacedFeature> resourceKey, ResourceKey<ConfiguredFeature<?, ?>> featureKey, List<PlacementModifier> placementModifiers) {
 		this.resourceKey = resourceKey;
 		this.featureKey = featureKey;
 		this.placementModifiers = placementModifiers;
+		this.placedFeature = new PlacedFeature(FrozenConfiguredFeatureUtils.getHolder(this.getFeatureKey()), this.placementModifiers);
 	}
 
 	public ResourceKey<PlacedFeature> getResourceKey() {
 		return this.resourceKey;
+	}
+
+	public PlacedFeature getPlacedFeature() {
+		return this.placedFeature;
 	}
 
 	public Holder<PlacedFeature> getHolder() {
