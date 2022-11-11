3a86bcad0d62bc72a3b4bbcfe27357bb5fb1a9ce
Treetrain1 <treetrain1@gmail.com>
2022-11-10 20:18:43 -0600

    merge

---
+++ src/main/java/net/frozenblock/lib/worldgen/feature/api/FrozenConfiguredFeatureUtils.java
@@ -0,0 +1,62 @@
+package net.frozenblock.lib.worldgen.feature.api;
+
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricWorldgenProvider;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.data.registries.VanillaRegistries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import org.jetbrains.annotations.NotNull;
+import org.quiltmc.qsl.frozenblock.core.registry.api.event.DynamicRegistryManagerSetupContext;
+
+public class FrozenConfiguredFeatureUtils {
+
+	public static ResourceKey<ConfiguredFeature<?, ?>> createKey(String namespace, String path) {
+		return ResourceKey.create(Registry.CONFIGURED_FEATURE_REGISTRY, new ResourceLocation(namespace, path));
+	}
+
+	public static Holder<? extends ConfiguredFeature<NoneFeatureConfiguration, ?>> register(DynamicRegistryManagerSetupContext context, DynamicRegistryManagerSetupContext.RegistryMap registries, String namespace, String id, Feature<NoneFeatureConfiguration> feature) {
+		return register(context, registries, namespace, id, feature, FeatureConfiguration.NONE);
+	}
+
+	public static <FC extends FeatureConfiguration, F extends Feature<FC>, C extends ConfiguredFeature<FC, ?>> Holder.Reference<C> register(DynamicRegistryManagerSetupContext context, DynamicRegistryManagerSetupContext.RegistryMap registries, @NotNull String namespace, @NotNull String id, F feature, @NotNull FC config) {
+		var configuredRegistry = registries.get(Registry.CONFIGURED_FEATURE_REGISTRY);
+		final ConfiguredFeature<FC, ?> configuredFeature = new ConfiguredFeature<>(feature, config);
+		Registry.register(configuredRegistry, new ResourceLocation(namespace, id), configuredFeature);
+		var featureEntry = getExact(registries, configuredFeature);
+		return (Holder.Reference<C>) featureEntry;
+	}
+
+	public static void register(
+			BootstapContext<ConfiguredFeature<?, ?>> bootstapContext, ResourceKey<ConfiguredFeature<?, ?>> registryKey, Feature<NoneFeatureConfiguration> feature
+	) {
+		FeatureUtils.register(bootstapContext, registryKey, feature, FeatureConfiguration.NONE);
+	}
+
+	public static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(
+			FabricWorldgenProvider.Entries entries, ResourceKey<ConfiguredFeature<?, ?>> registryKey, F feature, FC featureConfiguration
+	) {
+		entries.add(registryKey, new ConfiguredFeature<>(feature, featureConfiguration));
+	}
+
+	public static <FC extends FeatureConfiguration, V extends T, T extends ConfiguredFeature<FC, ?>> Holder.Reference<ConfiguredFeature<FeatureConfiguration, ?>> getExact(DynamicRegistryManagerSetupContext.RegistryMap registries, V value) {
+		var configuredRegistry = registries.get(Registry.CONFIGURED_FEATURE_REGISTRY);
+		var holder = configuredRegistry.getHolderOrThrow(configuredRegistry.getResourceKey(value).orElseThrow());
+		var exactHolder = getExactReference(holder);
+		return exactHolder;
+	}
+
+	public static <FC extends FeatureConfiguration, F extends Feature<FC>, V extends ConfiguredFeature<FC, ?>> Holder.Reference<V> getExactReference(Holder.Reference<?> reference) {
+		return (Holder.Reference<V>) reference;
+	}
+
+	public static Holder<ConfiguredFeature<?, ?>> getHolder(ResourceKey<ConfiguredFeature<?, ?>> resourceKey) {
+		return VanillaRegistries.createLookup().lookupOrThrow(Registry.CONFIGURED_FEATURE_REGISTRY).getOrThrow(resourceKey);
+	}
+}
---
+++ src/main/java/net/frozenblock/lib/worldgen/feature/api/FrozenPlacementUtils.java
@@ -0,0 +1,41 @@
+package net.frozenblock.lib.worldgen.feature.api;
+
+import java.util.List;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricWorldgenProvider;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.data.registries.VanillaRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+
+public class FrozenPlacementUtils {
+
+	public static ResourceKey<PlacedFeature> createKey(String namespace, String path) {
+		return ResourceKey.create(Registry.PLACED_FEATURE_REGISTRY, new ResourceLocation(namespace, path));
+	}
+
+	public static void register(
+			FabricWorldgenProvider.Entries entries,
+			ResourceKey<PlacedFeature> registryKey,
+			Holder<ConfiguredFeature<?, ?>> holder,
+			List<PlacementModifier> list
+	) {
+		entries.add(registryKey, new PlacedFeature(holder, List.copyOf(list)));
+	}
+
+	public static void register(
+			FabricWorldgenProvider.Entries entries,
+			ResourceKey<PlacedFeature> registryKey,
+			Holder<ConfiguredFeature<?, ?>> holder,
+			PlacementModifier... placementModifiers
+	) {
+		register(entries, registryKey, holder, List.of(placementModifiers));
+	}
+
+	public static Holder<PlacedFeature> getHolder(ResourceKey<PlacedFeature> resourceKey) {
+		return VanillaRegistries.createLookup().lookupOrThrow(Registry.PLACED_FEATURE_REGISTRY).getOrThrow(resourceKey);
+	}
+}
