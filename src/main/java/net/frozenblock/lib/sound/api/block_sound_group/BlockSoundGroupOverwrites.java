e370472c06d6398211a9211375b51d7fda511ec2
Treetrain1 <treetrain1@gmail.com>
2022-11-20 22:16:10 -0600

    move javadoc

--- src/main/java/net/frozenblock/lib/sound/api/block_sound_group/BlockSoundGroupOverwrites.java
+++ src/main/java/net/frozenblock/lib/sound/api/block_sound_group/BlockSoundGroupOverwrites.java
@@ -1,6 +1,7 @@
 package net.frozenblock.lib.sound.api.block_sound_group;
 
 import com.google.common.base.Preconditions;
+import it.unimi.dsi.fastutil.objects.Object2ReferenceOpenHashMap;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
@@ -9,9 +10,16 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Allows you to add any block by either adding its registry (Blocks.STONE) or its ID ("stone").
+ * If you want to add a modded block, make sure to put the nameSpaceID (wilderwild) in the first field, then the ID and soundGroup.
+ * Or you could just be normal and add the block itself instead of the ID.
+ * You can also add a LIST of blocks (IDs not allowed,) by using new Block[]{block1, block2}.
+ */
 public final class BlockSoundGroupOverwrites {
 
 	private BlockSoundGroupOverwrites() {
@@ -20,13 +28,6 @@ public final class BlockSoundGroupOverwrites {
 
 	public static final Map<ResourceLocation, SoundType> BLOCK_SOUNDS = new HashMap<>();
 	public static final Map<String, SoundType> NAMESPACE_SOUNDS = new HashMap<>();
-
-	/**
-	 * You can add any block by either adding its registry (Blocks.STONE) or its ID ("stone").
-	 * If you want to add a modded block, make sure to put the nameSpaceID (wilderwild) in the first field, then the ID and soundGroup.
-	 * Or you could just be normal and add the block itself instead of the ID.
-	 * You can also add a LIST of blocks (IDs not allowed,) by using new Block[]{block1, block2}.
-	 */
 
 	/**
 	 * This will only work with vanilla blocks.
