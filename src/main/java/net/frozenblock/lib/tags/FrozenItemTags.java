329ae96c9a3ad40df101c9131202e1f502044579
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-22 17:54:57 -0500

    Heavy Items

--- src/main/java/net/frozenblock/lib/tags/FrozenItemTags.java
+++ src/main/java/net/frozenblock/lib/tags/FrozenItemTags.java
@@ -23,6 +23,7 @@ public final class FrozenItemTags {
 	}
 
 	public static final TagKey<Item> NO_USE_GAME_EVENTS = of("dont_emit_use_game_events");
+	public static final TagKey<Item> HEAVY_ITEMS = of("heavy_items");
 
 	private static TagKey<Item> of(String path) {
 		return TagKey.create(Registry.ITEM_REGISTRY, FrozenMain.id(path));
