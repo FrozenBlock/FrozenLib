8572868c115e159378e0c970fde2168f5402f998
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-11 17:08:14 -0500

    items

--- src/main/java/net/frozenblock/lib/item/mixin/server/ItemCooldownsMixin.java
+++ src/main/java/net/frozenblock/lib/item/mixin/server/ItemCooldownsMixin.java
@@ -16,35 +16,30 @@
  * along with this program; if not, see <https://www.gnu.org/licenses/>.
  */
 
-package net.frozenblock.lib.mixin.server;
+package net.frozenblock.lib.item.mixin.server;
 
-import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.frozenblock.lib.FrozenMain;
+import java.util.Map;
 import net.frozenblock.lib.item.impl.CooldownInterface;
-import net.minecraft.core.Registry;
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemCooldowns;
-import net.minecraft.world.item.ServerItemCooldowns;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
-@Mixin(ServerItemCooldowns.class)
-public class ServerItemCooldownsMixin extends ItemCooldowns implements CooldownInterface {
+@Mixin(ItemCooldowns.class)
+public class ItemCooldownsMixin implements CooldownInterface {
+
+    @Final
     @Shadow
-	@Final
-    private ServerPlayer player;
+    public Map<Item, ItemCooldowns.CooldownInstance> cooldowns;
 
 	@Unique
 	@Override
     public void changeCooldown(Item item, int additional) {
         if (this.cooldowns.containsKey(item)) {
-            CooldownInstance cooldown = this.cooldowns.get(item);
-            this.cooldowns.put(item, new CooldownInstance(cooldown.startTime, cooldown.endTime + additional));
+            ItemCooldowns.CooldownInstance cooldown = this.cooldowns.get(item);
+            this.cooldowns.put(item, new ItemCooldowns.CooldownInstance(cooldown.startTime, cooldown.endTime + additional));
             this.onCooldownChanged(item, additional);
         }
     }
@@ -52,10 +47,6 @@ public class ServerItemCooldownsMixin extends ItemCooldowns implements CooldownI
 	@Unique
 	@Override
     public void onCooldownChanged(Item item, int additional) {
-        FriendlyByteBuf byteBuf = new FriendlyByteBuf(Unpooled.buffer());
-        byteBuf.writeId(Registry.ITEM, item);
-        byteBuf.writeVarInt(additional);
-        ServerPlayNetworking.send(this.player, FrozenMain.COOLDOWN_CHANGE_PACKET, byteBuf);
     }
 
 }
--- src/main/java/net/frozenblock/lib/item/mixin/server/ServerItemCooldownsMixin.java
+++ src/main/java/net/frozenblock/lib/item/mixin/server/ServerItemCooldownsMixin.java
@@ -16,32 +16,35 @@
  * along with this program; if not, see <https://www.gnu.org/licenses/>.
  */
 
-package net.frozenblock.lib.mixin.server;
+package net.frozenblock.lib.item.mixin.server;
 
-import com.google.common.collect.Maps;
-import java.util.Map;
+import io.netty.buffer.Unpooled;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.frozenblock.lib.FrozenMain;
 import net.frozenblock.lib.item.impl.CooldownInterface;
+import net.minecraft.core.Registry;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemCooldowns;
+import net.minecraft.world.item.ServerItemCooldowns;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
-@Mixin(ItemCooldowns.class)
-public class ItemCooldownsMixin implements CooldownInterface {
-
-    @Final
+@Mixin(ServerItemCooldowns.class)
+public class ServerItemCooldownsMixin extends ItemCooldowns implements CooldownInterface {
     @Shadow
-    public Map<Item, ItemCooldowns.CooldownInstance> cooldowns;
+	@Final
+    private ServerPlayer player;
 
 	@Unique
 	@Override
     public void changeCooldown(Item item, int additional) {
         if (this.cooldowns.containsKey(item)) {
-            ItemCooldowns.CooldownInstance cooldown = this.cooldowns.get(item);
-            this.cooldowns.put(item, new ItemCooldowns.CooldownInstance(cooldown.startTime, cooldown.endTime + additional));
+            CooldownInstance cooldown = this.cooldowns.get(item);
+            this.cooldowns.put(item, new CooldownInstance(cooldown.startTime, cooldown.endTime + additional));
             this.onCooldownChanged(item, additional);
         }
     }
@@ -49,6 +52,10 @@ public class ItemCooldownsMixin implements CooldownInterface {
 	@Unique
 	@Override
     public void onCooldownChanged(Item item, int additional) {
+        FriendlyByteBuf byteBuf = new FriendlyByteBuf(Unpooled.buffer());
+        byteBuf.writeId(Registry.ITEM, item);
+        byteBuf.writeVarInt(additional);
+        ServerPlayNetworking.send(this.player, FrozenMain.COOLDOWN_CHANGE_PACKET, byteBuf);
     }
 
 }
