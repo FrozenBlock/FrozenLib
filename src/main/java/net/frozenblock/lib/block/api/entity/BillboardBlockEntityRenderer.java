a04b4459cae8d088a4bf266769ef9f67651940a1
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-15 13:25:19 -0500

    bETTER

--- src/main/java/net/frozenblock/lib/block/api/entity/BillboardBlockEntityRenderer.java
+++ src/main/java/net/frozenblock/lib/block/api/entity/BillboardBlockEntityRenderer.java
@@ -22,33 +22,33 @@ import org.jetbrains.annotations.NotNull;
 
 @Environment(EnvType.CLIENT)
 public abstract class BillboardBlockEntityRenderer<T extends BlockEntity> implements BlockEntityRenderer<T> {
-    private final ModelPart base;
+	private final ModelPart base;
 
-    public BillboardBlockEntityRenderer(Context ctx) {
-        ModelPart root = this.getRoot();
-        this.base = root.getChild("base");
-    }
+	public BillboardBlockEntityRenderer(Context ctx) {
+		ModelPart root = this.getRoot(ctx);
+		this.base = root.getChild("base");
+	}
 
-    public static LayerDefinition getTexturedModelData() {
-        MeshDefinition modelData = new MeshDefinition();
-        PartDefinition modelPartData = modelData.getRoot();
-        modelPartData.addOrReplaceChild("base", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -16.0F, 0.0F, 16.0F, 16.0F, 0.0F), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, (float) Math.PI, 0.0F, 0.0F));
+	public static LayerDefinition getTexturedModelData() {
+		MeshDefinition modelData = new MeshDefinition();
+		PartDefinition modelPartData = modelData.getRoot();
+		modelPartData.addOrReplaceChild("base", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -16.0F, 0.0F, 16.0F, 16.0F, 0.0F), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, (float) Math.PI, 0.0F, 0.0F));
 		return LayerDefinition.create(modelData, 16, 16);
-    }
+	}
 
 	private final Quaternion rotation = new Quaternion(0F, 0F, 0F, 1F);
 
-    public void render(@NotNull T entity, float tickDelta, @NotNull PoseStack poseStack, @NotNull MultiBufferSource vertexConsumers, int light, int overlay) {
+	public void render(@NotNull T entity, float tickDelta, @NotNull PoseStack poseStack, @NotNull MultiBufferSource vertexConsumers, int light, int overlay) {
 		this.rotation.set(0.0f, 0.0f, 0.0f, 1.0f);
 		this.rotation.mul(Vector3f.YP.rotationDegrees(-Minecraft.getInstance().gameRenderer.getMainCamera().yRot));
 		poseStack.translate(0.5, 0, 0.5);
 		poseStack.pushPose();
 		poseStack.mulPose(this.rotation);
-		this.base.render(poseStack, vertexConsumers.getBuffer(RenderType.entityCutout(this.getTextureLocation(entity))), light, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
+		this.base.render(poseStack, vertexConsumers.getBuffer(RenderType.entityCutout(this.getTexture(entity))), light, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
 		poseStack.popPose();
-    }
+	}
 
-	public abstract ResourceLocation getTextureLocation(T entity);
+	public abstract ResourceLocation getTexture(T entity);
 
-	public abstract ModelPart getRoot();
+	public abstract ModelPart getRoot(Context ctx);
 }
