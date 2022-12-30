6610b6448a97063645872b0554b5a16e81baaf6f
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-30 14:39:38 -0500

    FrozenBlock Badge In ModMenu

---
+++ src/main/java/net/frozenblock/modmenu/mixin/plugin/RequiresModMenuMixinPlugin.java
@@ -0,0 +1,47 @@
+package net.frozenblock.modmenu.mixin.plugin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class RequiresModMenuMixinPlugin implements IMixinConfigPlugin {
+
+	@Override
+	public void onLoad(String mixinPackage) {
+
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		return FabricLoader.getInstance().getModContainer("modmenu").isPresent();
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+}
