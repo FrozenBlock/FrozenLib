a75c3aa56e26c17aba7853586537e56996402095
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-30 11:52:46 -0500

    fix

--- src/main/java/net/frozenblock/lib/FrozenClient.java
+++ src/main/java/net/frozenblock/lib/FrozenClient.java
@@ -400,6 +400,7 @@ public final class FrozenClient implements ClientModInitializer {
 					ClientWindManager.setSeed(seed);
 					ClientWindManager.overrideWind = override;
 					ClientWindManager.commandWind = new Vec3(xa, ya, za);
+					ClientWindManager.hasInitialized = true;
 				}
 			});
 		});
