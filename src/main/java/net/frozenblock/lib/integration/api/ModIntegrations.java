23ec3a333f21a605917857f54932742c04808556
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-01-25 21:21:55 -0500

    Update ModIntegrations.java

--- src/main/java/net/frozenblock/lib/integration/api/ModIntegrations.java
+++ src/main/java/net/frozenblock/lib/integration/api/ModIntegrations.java
@@ -25,6 +25,19 @@ public final class ModIntegrations {
         return Registry.register(FrozenRegistry.MOD_INTEGRATION, new ResourceLocation(srcModID, modID), new ModIntegrationSupplier<>(integration, modID));
     }
 
+	/**
+	 * Registers a mod integration class
+	 *
+	 * @param integration   The mod integration class to register
+	 * @param unloadedIntegration   The integration to use when the mod is unloaded
+	 * @param srcModID      The id of the mod registering the mod integration
+	 * @param modID      The id of the mod being integrated
+	 * @return A {@link ModIntegrationSupplier}.
+	 */
+	public static <T extends ModIntegration> ModIntegrationSupplier<T> register(Supplier<T> integration, Supplier<T> unloadedIntegration, String srcModID, String modID) {
+		return Registry.register(FrozenRegistry.MOD_INTEGRATION, new ResourceLocation(srcModID, modID), new ModIntegrationSupplier<>(integration, unloadedIntegration, modID));
+	}
+
     public static List<ModIntegrationSupplier> getIntegrationSuppliers() {
         return FrozenRegistry.MOD_INTEGRATION.stream().toList();
     }
