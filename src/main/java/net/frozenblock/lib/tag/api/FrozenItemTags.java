30b22cbf3f0d7b7db515c308f69ad4409139eda6
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-02-08 15:43:38 -0500

    Move cooldowns to FrozenLib

--- src/main/java/net/frozenblock/lib/tag/api/FrozenItemTags.java
+++ src/main/java/net/frozenblock/lib/tag/api/FrozenItemTags.java
@@ -31,6 +31,7 @@ public final class FrozenItemTags {
 
 	public static final TagKey<Item> NO_USE_GAME_EVENTS = of("dont_emit_use_game_events");
 	public static final TagKey<Item> HEAVY_ITEMS = of("heavy_items");
+	public static final TagKey<Item> ALWAYS_SAVE_COOLDOWNS = of("always_save_cooldowns");
 
 	private static TagKey<Item> of(String path) {
 		return TagKey.create(Registry.ITEM_REGISTRY, FrozenMain.id(path));
