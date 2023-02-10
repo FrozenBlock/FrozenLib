e1b47281cc972a4b20d9c5eebec9aa4dbb377a7c
Treetrain1 <treetrain1@gmail.com>
2023-02-09 19:08:07 -0600

    add registry

---
+++ src/main/java/net/frozenblock/lib/config/api/registry/ConfigRegistry.java
@@ -0,0 +1,14 @@
+package net.frozenblock.lib.config.api.registry;
+
+import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
+import net.fabricmc.fabric.api.event.registry.RegistryAttribute;
+import net.frozenblock.lib.FrozenMain;
+import net.frozenblock.lib.config.api.entry.TypedEntry;
+import net.minecraft.core.MappedRegistry;
+
+public class ConfigRegistry {
+
+	public static final MappedRegistry<TypedEntry> TYPED_ENTRY = FabricRegistryBuilder.createSimple(TypedEntry.class, FrozenMain.id("typed_entry"))
+			.attribute(RegistryAttribute.SYNCED)
+			.buildAndRegister();
+}
