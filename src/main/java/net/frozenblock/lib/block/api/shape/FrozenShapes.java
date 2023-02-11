65cc54362faa658eaca0bc1f4b4a375aacb95c89
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-02-11 14:31:14 -0500

    Update FrozenShapes.java

--- src/main/java/net/frozenblock/lib/block/api/shape/FrozenShapes.java
+++ src/main/java/net/frozenblock/lib/block/api/shape/FrozenShapes.java
@@ -15,7 +15,7 @@ public class FrozenShapes {
 	private static final VoxelShape NORTH_PLANE = Block.box(0.0, 0.0, 0.0, 16.0, 16.0, 1.0);
 	private static final VoxelShape SOUTH_PLANE = Block.box(0.0, 0.0, 15.0, 16.0, 16.0, 16.0);
 
-	private static final Map<Direction, VoxelShape> PLANE_BY_DIRECTION = Util.make(Maps.newEnumMap(Direction.class), shapes -> {
+	public static final Map<Direction, VoxelShape> PLANE_BY_DIRECTION = Util.make(Maps.newEnumMap(Direction.class), shapes -> {
 		shapes.put(Direction.NORTH, NORTH_PLANE);
 		shapes.put(Direction.EAST, EAST_PLANE);
 		shapes.put(Direction.SOUTH, SOUTH_PLANE);
@@ -23,4 +23,14 @@ public class FrozenShapes {
 		shapes.put(Direction.UP, UP_PLANE);
 		shapes.put(Direction.DOWN, DOWN_PLANE);
 	});
+
+	public static VoxelShape makePlaneFromDirection(Direction direction, float fromSide) {
+		double minX = direction.equals(Direction.EAST) ? 16F - fromSide : 0F;
+		double minY = direction.equals(Direction.UP) ? 16F - fromSide : 0F;
+		double minZ = direction.equals(Direction.SOUTH) ? 16F - fromSide : 0F;
+		double maxX = direction.equals(Direction.WEST) ? 0F + fromSide : 16F;
+		double maxY = direction.equals(Direction.DOWN) ? 0F + fromSide : 16F;
+		double maxZ = direction.equals(Direction.NORTH) ? 0F + fromSide : 16F;
+		return Block.box(minX, minY, minZ, maxX, maxY, maxZ);
+	}
 }
