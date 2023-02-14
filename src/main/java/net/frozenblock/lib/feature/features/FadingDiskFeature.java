a859e41631e6058f80d65bb9d14ebf4be3f4f333
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-02-13 20:02:30 -0500

    fading disk feature

---
+++ src/main/java/net/frozenblock/lib/feature/features/FadingDiskFeature.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2023 FrozenBlock
+ * This file is part of FrozenLib.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+package net.frozenblock.lib.feature.features;
+
+import com.mojang.serialization.Codec;
+import net.frozenblock.lib.feature.features.config.FadingDiskFeatureConfig;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.Heightmap.Types;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+
+public class FadingDiskFeature extends Feature<FadingDiskFeatureConfig> {
+    public FadingDiskFeature(Codec<FadingDiskFeatureConfig> codec) {
+        super(codec);
+    }
+
+    public boolean place(FeaturePlaceContext<FadingDiskFeatureConfig> context) {
+        boolean bl = false;
+        BlockPos blockPos = context.origin();
+        WorldGenLevel level = context.level();
+		FadingDiskFeatureConfig config = context.config();
+        BlockPos s = blockPos.atY(level.getHeight(Types.MOTION_BLOCKING_NO_LEAVES, blockPos.getX(), blockPos.getZ()));
+        RandomSource random = level.getRandom();
+        int radius = random.nextIntBetweenInclusive(4, 10);
+        //DISK
+        BlockPos.MutableBlockPos mutableDisk = s.mutable();
+        int bx = s.getX();
+        int bz = s.getZ();
+		int y = s.getY();
+        for (int x = bx - radius; x <= bx + radius; x++) {
+            for (int z = bz - radius; z <= bz + radius; z++) {
+                double distance = ((bx - x) * (bx - x) + ((bz - z) * (bz - z)));
+                if (distance < radius * radius) {
+                    mutableDisk.set(x, y, z);
+					boolean inner = !mutableDisk.closerThan(s, radius * 0.4);
+                    boolean fade = !inner && !mutableDisk.closerThan(s, radius * 0.8);
+					mutableDisk.set(blockPos.atY(level.getHeight(Types.MOTION_BLOCKING_NO_LEAVES, x, z)));
+					if (level.getBlockState(mutableDisk.below()).is(Blocks.WATER) && level.getFluidState(mutableDisk).isEmpty() && level.getBlockState(mutableDisk).isAir()) {
+                        if (random.nextFloat() > 0.2F) {
+							if (fade) {
+                                if (random.nextFloat() > 0.5F) {
+                                    level.setBlock(mutableDisk, config.outerState.getState(random, mutableDisk), 3);
+                                    bl = true;
+                                }
+                            } else {
+                                level.setBlock(mutableDisk, !inner ? config.outerState.getState(random, mutableDisk) : config.innerState.getState(random, mutableDisk), 3);
+                                bl = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return bl;
+    }
+
+}
---
+++ src/main/java/net/frozenblock/lib/feature/features/config/FadingDiskFeatureConfig.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 FrozenBlock
+ * This file is part of FrozenLib.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+package net.frozenblock.lib.feature.features.config;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+
+public class FadingDiskFeatureConfig implements FeatureConfiguration {
+    public static final Codec<FadingDiskFeatureConfig> CODEC = RecordCodecBuilder.create(
+            (instance) -> instance.group(
+					BlockStateProvider.CODEC.fieldOf("innerState").forGetter(config -> config.innerState),
+					BlockStateProvider.CODEC.fieldOf("outerState").forGetter(config -> config.outerState),
+					IntProvider.CODEC.fieldOf("radius").forGetter(config -> config.radius)
+			).apply(instance, FadingDiskFeatureConfig::new)
+    );
+
+    public final BlockStateProvider innerState;
+    public final BlockStateProvider outerState;
+    public final IntProvider radius;
+
+    public FadingDiskFeatureConfig(BlockStateProvider innerState, BlockStateProvider outerState, IntProvider radius) {
+		this.innerState = innerState;
+		this.outerState = outerState;
+		this.radius = radius;
+    }
+}
