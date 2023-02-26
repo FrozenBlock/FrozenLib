db2aa7d85791cb8ccea921959e865aad68eccc51
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-02-26 13:49:34 -0500

    goth

---
+++ src/main/java/net/frozenblock/lib/item/api/ItemBlockStateTagUtils.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 FrozenBlock
+ * This file is part of FrozenLib.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.frozenblock.lib.item.api;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+
+public class ItemBlockStateTagUtils {
+
+	public static int getIntProperty(ItemStack stack, String property) {
+		if (stack.getTag() != null) {
+			CompoundTag stateTag = stack.getTag().getCompound("BlockStateTag");
+			if (stateTag != null) {
+				return Integer.parseInt(stateTag.getString(property));
+			}
+		}
+		return 0;
+	}
+
+	public static boolean getBoolProperty(ItemStack stack, String property) {
+		if (stack.getTag() != null) {
+			CompoundTag stateTag = stack.getTag().getCompound("BlockStateTag");
+			if (stateTag != null) {
+				return stateTag.getString(property).equals("true");
+			}
+		}
+		return false;
+	}
+
+}
