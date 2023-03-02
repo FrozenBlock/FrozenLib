e671a92b748cefd1193a75dc4d310b3f6a264d0a
Treetrain1 <treetrain1@gmail.com>
2023-03-01 20:11:23 -0600

    merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -203,7 +203,7 @@ dependencies {
 	mappings(loom.layered {
 		// please annoy treetrain if this doesnt work
 		mappings("org.quiltmc:quilt-mappings:${minecraft_version}+build.${quilt_mappings}:intermediary-v2")
-        parchment("org.parchmentmc.data:parchment-1.19.2:${parchment_mappings}@zip")
+        parchment("org.parchmentmc.data:parchment-1.19.3:${parchment_mappings}@zip")
 		officialMojangMappings {
 			nameSyntheticMembers = false
 		}
