4c66b4cb8cbd456c14a844280932fd67539f24ce
Treetrain1 <treetrain1@gmail.com>
2023-03-06 20:52:41 -0600

    simplify

--- src/main/java/net/frozenblock/lib/worldgen/feature/api/FrozenConfiguredFeature.java
+++ src/main/java/net/frozenblock/lib/worldgen/feature/api/FrozenConfiguredFeature.java
@@ -2,11 +2,13 @@ package net.frozenblock.lib.worldgen.feature.api;
 
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
+import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import java.util.Optional;
 
@@ -33,5 +35,11 @@ public class FrozenConfiguredFeature<FC extends FeatureConfiguration, C extends 
 	public FrozenConfiguredFeature<FC, C> setHolder(Holder<C> holder) {
 		this.holder = holder;
 		return this;
+	}
+
+	@SuppressWarnings("unchecked")
+	public <F extends Feature<FC>> FrozenConfiguredFeature<FC, C> makeAndSetHolder(@NotNull F feature, @NotNull FC config) {
+		Holder<C> holder = (Holder<C>) FeatureUtils.register(this.getKey().location().toString(), feature, config);
+		return this.setHolder(holder);
 	}
 }
