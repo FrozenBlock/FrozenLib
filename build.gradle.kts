0fcb28e92ded9de6bc0403403ce9a4fa45022f0b
Treetrain1 <treetrain1@gmail.com>
2023-03-14 19:48:35 -0500

    use base name

--- build.gradle.kts
+++ build.gradle.kts
@@ -415,7 +415,7 @@ publishing {
 
                 pom {
                     groupId = publishGroup
-                    artifactId = rootProject.name.trim(' ')
+                    artifactId = rootProject.base.archivesName.get().lowercase()
                     version = publishVersion
                     withXml {
                         asNode().appendNode("properties").appendNode("hash", hash)
