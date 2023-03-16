a0f63b551c9e2a53a4755fda709f24833f7a56ac
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-03-16 11:10:28 -0400

    separate two mixins

--- src/main/java/net/frozenblock/lib/sound/mixin/LivingEntityMixin.java
+++ src/main/java/net/frozenblock/lib/sound/mixin/LivingEntityMixin.java
@@ -23,64 +23,26 @@ import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.frozenblock.lib.FrozenMain;
 import net.frozenblock.lib.sound.api.damagesource.PlayerDamageSourceSounds;
-import net.frozenblock.lib.tag.api.FrozenItemTags;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityEvent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-
-    @Shadow
-    protected ItemStack useItem;
-    @Shadow
-    protected int useItemRemaining;
-
-    @Inject(method = "startUsingItem", at = @At("HEAD"), cancellable = true)
-    private void frozenLib$preventStartingGameEvent(InteractionHand hand, CallbackInfo info) {
-        LivingEntity entity = LivingEntity.class.cast(this);
-        ItemStack stack = entity.getItemInHand(hand);
-        if (!stack.isEmpty() && !entity.isUsingItem()) {
-            if (stack.is(FrozenItemTags.NO_USE_GAME_EVENTS)) {
-                info.cancel();
-                this.useItem = stack;
-                this.useItemRemaining = stack.getUseDuration();
-                if (!entity.level.isClientSide) {
-                    this.setLivingEntityFlag(1, true);
-                    this.setLivingEntityFlag(2, hand == InteractionHand.OFF_HAND);
-                }
-            }
-        }
-    }
-
-    @Inject(method = "stopUsingItem", at = @At("HEAD"), cancellable = true)
-    public void frozenLib$preventStoppingGameEvent(CallbackInfo info) {
-        LivingEntity entity = LivingEntity.class.cast(this);
-        if (!entity.level.isClientSide) {
-            ItemStack stack = entity.getUseItem();
-            if (stack.is(FrozenItemTags.NO_USE_GAME_EVENTS)) {
-                this.setLivingEntityFlag(1, false);
-                info.cancel();
-            }
-        }
-    }
 
     @Inject(method = "hurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;broadcastEntityEvent(Lnet/minecraft/world/entity/Entity;B)V", ordinal = 2), locals = LocalCapture.CAPTURE_FAILHARD)
     private void hurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir, float f, boolean bl, float g, boolean bl2, Entity entity2, byte event) {
@@ -102,11 +64,6 @@ public abstract class LivingEntityMixin {
 			return null;
 		}
 		return this.getHurtSound(par2);
-	}
-
-	@Shadow
-	protected void setLivingEntityFlag(int mask, boolean value) {
-		throw new UnsupportedOperationException("Mixin injection failed. - FrozenLib LivingEntityMixin.");
 	}
 
 	@Shadow
