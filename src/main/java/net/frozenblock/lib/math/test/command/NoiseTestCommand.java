a6a703908c590408619796d1b31eab19381089c1
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-03-17 14:44:18 -0400

    Revert "better noise test command"
    This reverts commit baf6f5f7430dddab20907a7efb362273dbe0bcb0.

--- src/main/java/net/frozenblock/lib/math/test/command/NoiseTestCommand.java
+++ src/main/java/net/frozenblock/lib/math/test/command/NoiseTestCommand.java
@@ -19,7 +19,6 @@
 package net.frozenblock.lib.math.test.command;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.DoubleArgumentType;
 import net.frozenblock.lib.math.api.EasyNoiseSampler;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
@@ -28,12 +27,11 @@ import net.minecraft.network.chat.Component;
 public class NoiseTestCommand { //Used to profile noises
 
 	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-		dispatcher.register(Commands.literal("noisetest").requires(source -> source.hasPermission(2)).executes(context -> noiseTest(context.getSource(), 100))
-				.then(Commands.argument("testlength", DoubleArgumentType.doubleArg()).executes(context -> noiseTest(context.getSource(), DoubleArgumentType.getDouble(context,"testlength")))));
+		dispatcher.register(Commands.literal("noisetest").requires(source -> source.hasPermission(2)).executes(context -> noiseTest(context.getSource())));
 	}
 
-	private static int noiseTest(CommandSourceStack source, double testlength) {
-		EasyNoiseSampler.sampleTest(testlength);
+	private static int noiseTest(CommandSourceStack source) {
+		EasyNoiseSampler.sampleTest();
 		source.sendSuccess(Component.literal("CHECK LOGS FOR NOISE SPEEDS"), true);
 		return 1;
 	}
