2e66e1eaa2f056b0446c7786c40435a5a34e771d
Treetrain1 <treetrain1@gmail.com>
2023-03-23 11:00:04 -0500

    Provide mod container

--- src/main/java/net/frozenblock/lib/entrypoint/api/FrozenModInitializer.java
+++ src/main/java/net/frozenblock/lib/entrypoint/api/FrozenModInitializer.java
@@ -1,26 +1,34 @@
 package net.frozenblock.lib.entrypoint.api;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 
 public abstract class FrozenModInitializer implements ModInitializer {
 
 	private final String modId;
+	private final ModContainer container;
 
 	public FrozenModInitializer(String modId) {
 		this.modId = modId;
+		this.container = FabricLoader.getInstance().getModContainer(modId).orElseThrow(() -> new IllegalStateException("Mod container not found for mod id " + modId));
 	}
 
 	@Override
 	public void onInitialize() {
-		this.onInitialize(this.modId);
+		this.onInitialize(this.modId, this.container);
 	}
 
-	public abstract void onInitialize(String modId);
+	public abstract void onInitialize(String modId, ModContainer container);
 
 	public String modId() {
 		return this.modId;
+	}
+
+	public ModContainer container() {
+		return this.container;
 	}
 
 	public ResourceLocation id(String path) {
