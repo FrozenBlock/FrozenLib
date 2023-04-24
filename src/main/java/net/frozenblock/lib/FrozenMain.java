da3041b6fd59e3c1696bd4c9d9236be42d4ab612
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-04-24 17:04:39 -0400

    Built In Loot Table Registry

--- src/main/java/net/frozenblock/lib/FrozenMain.java
+++ src/main/java/net/frozenblock/lib/FrozenMain.java
@@ -28,14 +28,13 @@ import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
 import net.frozenblock.lib.entrypoint.api.FrozenMainEntrypoint;
 import net.frozenblock.lib.event.api.PlayerJoinEvents;
 import net.frozenblock.lib.ingamedevtools.RegisterInGameDevTools;
+import net.frozenblock.lib.loottable.api.BuiltInLootTablesEntrypoint;
 import net.frozenblock.lib.worldgen.feature.api.FrozenFeatures;
 import net.frozenblock.lib.registry.api.FrozenRegistry;
 import net.frozenblock.lib.screenshake.api.ScreenShakeManager;
 import net.frozenblock.lib.screenshake.api.command.ScreenShakeCommand;
 import net.frozenblock.lib.screenshake.impl.ScreenShakeStorage;
 import net.frozenblock.lib.sound.api.predicate.SoundPredicate;
-import net.frozenblock.lib.sound.impl.EntityLoopingFadingDistanceSoundInterface;
-import net.frozenblock.lib.sound.impl.EntityLoopingSoundInterface;
 import net.frozenblock.lib.spotting_icons.api.SpottingIconPredicate;
 import net.frozenblock.lib.wind.api.WindManager;
 import net.frozenblock.lib.wind.api.command.WindOverrideCommand;
@@ -45,7 +44,6 @@ import net.frozenblock.lib.worldgen.surface.api.FrozenSurfaceRuleEntrypoint;
 import net.frozenblock.lib.worldgen.surface.impl.BiomeTagConditionSource;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.DimensionDataStorage;
 import org.quiltmc.qsl.frozenblock.misc.datafixerupper.impl.ServerFreezer;
@@ -67,6 +65,7 @@ public final class FrozenMain implements ModInitializer {
 	public static boolean UNSTABLE_LOGGING = FabricLoader.getInstance().isDevelopmentEnvironment();
 
 	public static final List<EntrypointContainer<FrozenSurfaceRuleEntrypoint>> SURFACE_RULE_ENTRYPOINTS = FabricLoader.getInstance().getEntrypointContainers("frozenlib:surfacerules", FrozenSurfaceRuleEntrypoint.class);
+	public static final List<EntrypointContainer<BuiltInLootTablesEntrypoint>> LOOT_TABLE_ENTRYPOINTS = FabricLoader.getInstance().getEntrypointContainers("frozenlib:loottables", BuiltInLootTablesEntrypoint.class);
 
 	@Override
 	public void onInitialize() {
@@ -167,5 +166,5 @@ public final class FrozenMain implements ModInitializer {
 			LOGGER.error(string);
 		}
 	}
-	
+
 }
