fd5ba0b9471ba0a3525994f1c9b63429b69e5a48
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-05-27 16:06:40 -0400

    Fix Disc Feature Crashes

--- src/main/java/net/frozenblock/lib/worldgen/surface/impl/BiomeTagConditionSource.java
+++ src/main/java/net/frozenblock/lib/worldgen/surface/impl/BiomeTagConditionSource.java
@@ -24,19 +24,21 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.util.KeyDispatchDataCodec;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.SurfaceRules;
+import org.jetbrains.annotations.NotNull;
 
 public final class BiomeTagConditionSource implements SurfaceRules.ConditionSource {
 	public static final KeyDispatchDataCodec<BiomeTagConditionSource> CODEC = KeyDispatchDataCodec.of(RecordCodecBuilder.mapCodec((instance) ->
 			instance.group(
 					TagKey.codec(Registries.BIOME)
-							.fieldOf("biome_tag")
-							.forGetter(BiomeTagConditionSource::getBiomeTagKey))
-					.apply(instance, BiomeTagConditionSource::new)
-			)
+						.fieldOf("biome_tag")
+						.forGetter(BiomeTagConditionSource::getBiomeTagKey))
+				.apply(instance, BiomeTagConditionSource::new)
+		)
 	);
 
 	private final TagKey<Biome> biomeTagKey;
 
+	@NotNull
 	public static BiomeTagConditionSource isBiomeTag(TagKey<Biome> biomeTagKey) {
 		return new BiomeTagConditionSource(biomeTagKey);
 	}
@@ -45,16 +47,21 @@ public final class BiomeTagConditionSource implements SurfaceRules.ConditionSour
 		this.biomeTagKey = biomeTagKey;
 	}
 
+	@Override
+	@NotNull
 	public KeyDispatchDataCodec<? extends SurfaceRules.ConditionSource> codec() {
 		return CODEC;
 	}
 
+	@Override
+	@NotNull
 	public SurfaceRules.Condition apply(SurfaceRules.Context context) {
 		class BiomeTagCondition extends SurfaceRules.LazyYCondition {
-			BiomeTagCondition(SurfaceRules.Context context) {
+			BiomeTagCondition(@NotNull SurfaceRules.Context context) {
 				super(context);
 			}
 
+			@Override
 			protected boolean compute() {
 				return this.context.biome.get().is(BiomeTagConditionSource.this.biomeTagKey);
 			}
@@ -63,6 +70,7 @@ public final class BiomeTagConditionSource implements SurfaceRules.ConditionSour
 		return new BiomeTagCondition(context);
 	}
 
+	@Override
 	public boolean equals(Object object) {
 		if (this == object) {
 			return true;
@@ -73,10 +81,13 @@ public final class BiomeTagConditionSource implements SurfaceRules.ConditionSour
 		}
 	}
 
+	@Override
 	public int hashCode() {
 		return this.biomeTagKey.hashCode();
 	}
 
+	@Override
+	@NotNull
 	public String toString() {
 		return "BiomeConditionSource[biomeTagKey=" + this.biomeTagKey + "]";
 	}
