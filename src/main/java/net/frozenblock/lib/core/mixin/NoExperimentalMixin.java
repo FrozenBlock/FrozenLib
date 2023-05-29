af1a0e977b3f19dc593ff5aeb2de14f1eb92448e
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-05-29 16:00:50 -0400

    1.20-pre7

--- src/main/java/net/frozenblock/lib/core/mixin/NoExperimentalMixin.java
+++ src/main/java/net/frozenblock/lib/core/mixin/NoExperimentalMixin.java
@@ -18,19 +18,8 @@
 
 package net.frozenblock.lib.core.mixin;
 
-import com.mojang.datafixers.DataFixer;
 import com.mojang.serialization.Lifecycle;
-import java.util.Set;
-import java.util.UUID;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.level.LevelSettings;
-import net.minecraft.world.level.border.WorldBorder;
-import net.minecraft.world.level.levelgen.WorldGenSettings;
-import net.minecraft.world.level.levelgen.WorldOptions;
 import net.minecraft.world.level.storage.PrimaryLevelData;
-import net.minecraft.world.level.timers.TimerQueue;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -47,15 +36,9 @@ public class NoExperimentalMixin {
     @Mutable
     private Lifecycle worldGenSettingsLifecycle;
 
-    @Inject(method = "<init>(Lcom/mojang/datafixers/DataFixer;ILnet/minecraft/nbt/CompoundTag;ZIIIFJJIIIZIZZZLnet/minecraft/world/level/border/WorldBorder$Settings;IILjava/util/UUID;Ljava/util/Set;Ljava/util/Set;Lnet/minecraft/world/level/timers/TimerQueue;Lnet/minecraft/nbt/CompoundTag;Lnet/minecraft/nbt/CompoundTag;Lnet/minecraft/world/level/LevelSettings;Lnet/minecraft/world/level/levelgen/WorldOptions;Lnet/minecraft/world/level/storage/PrimaryLevelData$SpecialWorldProperty;Lcom/mojang/serialization/Lifecycle;)V", at = @At("TAIL"))
-    private void init(DataFixer dataFixer, int i, CompoundTag compoundTag, boolean bl, int j, int k, int l, float f, long m, long n, int o, int p, int q, boolean bl2, int r, boolean bl3, boolean bl4, boolean bl5, WorldBorder.Settings settings, int s, int t, UUID uuid, Set set, Set set2, TimerQueue timerQueue, CompoundTag compoundTag2, CompoundTag compoundTag3, LevelSettings levelSettings, WorldOptions worldOptions, PrimaryLevelData.SpecialWorldProperty specialWorldProperty, Lifecycle lifecycle, CallbackInfo ci) {
+    @Inject(method = "<init>*", at = @At("TAIL"))
+    private void init(CallbackInfo info) {
         this.worldGenSettingsLifecycle = Lifecycle.stable();
     }
-
-    /*@Inject(method = "worldGenSettingsLifecycle", at = @At("HEAD"), cancellable = true)
-    public void worldGenSettingsLifecycle(CallbackInfoReturnable<Lifecycle> info) {
-        info.cancel();
-        info.setReturnValue(Lifecycle.stable());
-    }*/
 
 }
