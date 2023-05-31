8322babd741cbf2085f6448edb82ed99b899b517
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-05-31 15:47:52 -0400

    Revert "no license nonsense"
    This reverts commit cc7f2ad42bcbdc30856557ab6eb2ffca541efa1b.

--- build.gradle.kts
+++ build.gradle.kts
@@ -296,6 +296,13 @@ tasks {
         useJUnitPlatform()
     }
 
+    license {
+        rule(file("codeformat/QUILT_MODIFIED_HEADER"))
+        rule(file("codeformat/HEADER"))
+
+        include("**//*.java")
+    }
+
     register("javadocJar", Jar::class) {
         dependsOn(javadoc)
         archiveClassifier.set("javadoc")
