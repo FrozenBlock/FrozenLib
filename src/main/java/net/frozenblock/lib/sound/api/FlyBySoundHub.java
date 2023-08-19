950345f4ff7f7b33ff42981385da4a0a039f974c
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-08-19 01:38:24 -0400

    why wont it work

--- src/main/java/net/frozenblock/lib/sound/api/FlyBySoundHub.java
+++ src/main/java/net/frozenblock/lib/sound/api/FlyBySoundHub.java
@@ -26,6 +26,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.sounds.EntityBoundSoundInstance;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
@@ -36,52 +37,62 @@ import net.minecraft.world.phys.Vec3;
 public class FlyBySoundHub {
 
     public static final Map<EntityType<?>, FlyBySound> AUTO_ENTITIES_AND_SOUNDS = new Object2ObjectOpenHashMap<>();
-
     public static final Map<Entity, FlyBySound> FLYBY_ENTITIES_AND_SOUNDS = new Object2ObjectOpenHashMap<>();
     public static final Map<Entity, Integer> ENTITY_COOLDOWNS = new Object2ObjectOpenHashMap<>();
 
-    public static void update(Minecraft client, Player player, boolean autoSounds) {
-        for (Entity entity : FLYBY_ENTITIES_AND_SOUNDS.keySet()) {
-            if (client.level == null) {
-                FLYBY_ENTITIES_AND_SOUNDS.clear();
-                return;
-            }
-            if (entity != null) {
-                Vec3 vel = entity.getDeltaMovement();
-                Vec3 playerVel = player.getDeltaMovement();
-                Vec3 entityPos = entity.position();
-                Vec3 playerPos = player.getEyePosition();
-                double distanceTo = entityPos.distanceTo(playerPos);
-                double newDistanceTo = entityPos.add(vel.scale(2)).distanceTo(playerPos.add(playerVel));
-
-                int cooldown = ENTITY_COOLDOWNS.getOrDefault(entity, 0) - 1;
-                ENTITY_COOLDOWNS.put(entity, cooldown);
-                if ((distanceTo > newDistanceTo && distanceTo < ((vel.lengthSqr() + playerVel.length()) * 2)) && cooldown <= 0) {
-                    FlyBySound flyBy = FLYBY_ENTITIES_AND_SOUNDS.get(entity);
-                    float volume = (float) (flyBy.volume + (vel.length() / 2));
-                    client.getSoundManager().play(new EntityBoundSoundInstance(flyBy.sound, flyBy.category, volume, flyBy.pitch, entity, client.level.random.nextLong()));
-                    ENTITY_COOLDOWNS.put(entity, 10);
-                }
-            }
-        }
-        //Remove Entities That Aren't Active
-        for (Entity entity : FLYBY_ENTITIES_AND_SOUNDS.keySet().stream().toList()) {
-            if (entity == null || entity.isRemoved() || entity.isSilent() || (entity.distanceTo(client.getCameraEntity()) > 16 && !AUTO_ENTITIES_AND_SOUNDS.containsKey(entity.getType()))) {
-                FLYBY_ENTITIES_AND_SOUNDS.remove(entity);
-            }
-        }
-
-        if (!AUTO_ENTITIES_AND_SOUNDS.isEmpty()) {
-			if (client.level != null && autoSounds) {
-				AABB box = new AABB(player.getEyePosition().add(-2, -2, -2), player.getEyePosition().add(2, 2, 2));
-				for (Entity entity : client.level.getEntities(player, box)) {
-					EntityType<?> type = entity.getType();
-					if (AUTO_ENTITIES_AND_SOUNDS.containsKey(type)) {
-						addEntity(entity, AUTO_ENTITIES_AND_SOUNDS.get(type));
+    public static void update(Minecraft client, Entity cameraEntity, boolean autoSounds) {
+		if (client.level != null && cameraEntity != null) {
+			Vec3 playerPos = cameraEntity.getEyePosition();
+			double playerBBWidth = cameraEntity.getBbWidth() * 2;
+			AABB playerHeadBox = new AABB(cameraEntity.getEyePosition().add(-playerBBWidth, -playerBBWidth, -playerBBWidth), cameraEntity.getEyePosition().add(playerBBWidth, playerBBWidth, playerBBWidth));
+			for (Entity entity : FLYBY_ENTITIES_AND_SOUNDS.keySet()) {
+				if (client.level == null) {
+					FLYBY_ENTITIES_AND_SOUNDS.clear();
+					return;
+				}
+				if (entity != null) {
+					Vec3 entityVelocity = entity.getPosition(1F).subtract(entity.getPosition(0F));
+					double entityVelocityLength = entityVelocity.length();
+					AABB entityBox = entity.getBoundingBox().inflate(1D + (entityVelocityLength * 5));
+					if (playerHeadBox.intersects(entityBox)) {
+						if (entityVelocityLength > 0.05) {
+							Vec3 entityPos = entity.position();
+							double distanceTo = entityPos.distanceTo(playerPos);
+							double newDistanceTo = entityPos.add(entityVelocity.scale(2)).distanceTo(playerPos);
+							int cooldown = ENTITY_COOLDOWNS.getOrDefault(entity, 0) - 1;
+							ENTITY_COOLDOWNS.put(entity, cooldown);
+							if (distanceTo > newDistanceTo && newDistanceTo < entityVelocityLength * 2 && cooldown <= 0) {
+								double deltaDistance = distanceTo - newDistanceTo;
+								if (deltaDistance > 0.05) {
+									FlyBySound flyBy = FLYBY_ENTITIES_AND_SOUNDS.get(entity);
+									float volume = (float) (flyBy.volume + (deltaDistance));
+									client.getSoundManager().play(new EntityBoundSoundInstance(flyBy.sound, flyBy.category, volume, flyBy.pitch, entity, client.level.random.nextLong()));
+									ENTITY_COOLDOWNS.put(entity, 20);
+								}
+							}
+						}
 					}
 				}
-            }
-        }
+			}
+
+			//Remove Entities That Aren't Active
+			for (Entity entity : FLYBY_ENTITIES_AND_SOUNDS.keySet().stream().toList()) {
+				if (entity == null || entity.isRemoved() || entity.isSilent() || (entity.distanceTo(entity) > 16)) {
+					FLYBY_ENTITIES_AND_SOUNDS.remove(entity);
+				}
+			}
+
+			if (!AUTO_ENTITIES_AND_SOUNDS.isEmpty()) {
+				if (client.level != null && autoSounds) {
+					for (Entity entity : client.level.getEntities(cameraEntity, playerHeadBox)) {
+						EntityType<?> type = entity.getType();
+						if (AUTO_ENTITIES_AND_SOUNDS.containsKey(type)) {
+							addEntity(entity, AUTO_ENTITIES_AND_SOUNDS.get(type));
+						}
+					}
+				}
+			}
+		}
     }
 
     public static void addEntity(Entity entity, FlyBySound flyBySound) {
