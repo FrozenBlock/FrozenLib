d03a81fa0487156322dca6ce3c95c132e324442f
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-08-25 12:09:05 -0500

    random flyby sound stuff

--- src/main/java/net/frozenblock/lib/sound/api/FlyBySoundHub.java
+++ src/main/java/net/frozenblock/lib/sound/api/FlyBySoundHub.java
@@ -33,7 +33,12 @@ import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
 @Environment(EnvType.CLIENT)
-public class FlyBySoundHub {
+public final class FlyBySoundHub {
+
+    private static final int PLAY_COOLDOWN = 40;
+    private static final int MIN_DISTANCE_FOR_REMOVAL = 16;
+    private static final int AUTO_ENTITY_DISTANCE = 3;
+    private static final int AUTO_ENTITY_COOLDOWN = 1;
 
     public static final Map<EntityType<?>, FlyBySound> AUTO_ENTITIES_AND_SOUNDS = new Object2ObjectOpenHashMap<>();
 
@@ -42,11 +47,12 @@ public class FlyBySoundHub {
     private static int checkAroundCooldown;
 
     public static void update(Minecraft client, Player player, boolean autoSounds) {
+        if (client.level == null) {
+            FLYBY_ENTITIES_AND_SOUNDS.clear();
+            ENTITY_COOLDOWNS.clear();
+            return;
+        }
         for (Entity entity : FLYBY_ENTITIES_AND_SOUNDS.keySet()) {
-            if (client.level == null) {
-                FLYBY_ENTITIES_AND_SOUNDS.clear();
-                return;
-            }
             if (entity != null) {
                 Vec3 vel = entity.getDeltaMovement();
                 Vec3 playerVel = player.getDeltaMovement();
@@ -61,13 +67,13 @@ public class FlyBySoundHub {
                     FlyBySound flyBy = FLYBY_ENTITIES_AND_SOUNDS.get(entity);
                     float volume = (float) (flyBy.volume + (vel.length() / 2));
                     client.getSoundManager().play(new EntityBoundSoundInstance(flyBy.sound, flyBy.category, volume, flyBy.pitch, entity, client.level.random.nextLong()));
-                    ENTITY_COOLDOWNS.put(entity, 40);
+                    ENTITY_COOLDOWNS.put(entity, PLAY_COOLDOWN);
                 }
             }
         }
         //Remove Entities That Aren't Active
         for (Entity entity : FLYBY_ENTITIES_AND_SOUNDS.keySet().stream().toList()) {
-            if (entity == null || entity.isRemoved() || entity.isSilent() || (entity.distanceTo(client.getCameraEntity()) > 16 && !AUTO_ENTITIES_AND_SOUNDS.containsKey(entity.getType()))) {
+            if (entity == null || entity.isRemoved() || entity.isSilent() || (entity.distanceTo(client.getCameraEntity()) > MIN_DISTANCE_FOR_REMOVAL && !AUTO_ENTITIES_AND_SOUNDS.containsKey(entity.getType()))) {
                 FLYBY_ENTITIES_AND_SOUNDS.remove(entity);
             }
         }
@@ -75,15 +81,13 @@ public class FlyBySoundHub {
         if (!AUTO_ENTITIES_AND_SOUNDS.isEmpty()) {
             if (checkAroundCooldown > 0) {
                 --checkAroundCooldown;
-            } else {
-                if (client.level != null && autoSounds) {
-                    checkAroundCooldown = 1;
-                    AABB box = new AABB(player.blockPosition().offset(-3, -3, -3), player.blockPosition().offset(3, 3, 3));
-                    for (Entity entity : client.level.getEntities(player, box)) {
-                        EntityType<?> type = entity.getType();
-                        if (AUTO_ENTITIES_AND_SOUNDS.containsKey(type)) {
-                            addEntity(entity, AUTO_ENTITIES_AND_SOUNDS.get(type));
-                        }
+            } else if (autoSounds) {
+                checkAroundCooldown = AUTO_ENTITY_COOLDOWN;
+                AABB box = new AABB(player.blockPosition().offset(-AUTO_ENTITY_DISTANCE, -AUTO_ENTITY_DISTANCE, -AUTO_ENTITY_DISTANCE), player.blockPosition().offset(AUTO_ENTITY_DISTANCE, AUTO_ENTITY_DISTANCE, AUTO_ENTITY_DISTANCE));
+                for (Entity entity : client.level.getEntities(player, box)) {
+                    EntityType<?> type = entity.getType();
+                    if (AUTO_ENTITIES_AND_SOUNDS.containsKey(type)) {
+                        addEntity(entity, AUTO_ENTITIES_AND_SOUNDS.get(type));
                     }
                 }
             }
