8000d2313e6dddaefedd5a96b2a0037f9d8cfda0
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-09-08 10:56:58 -0500

    update NoInteractionStorage

--- src/main/java/net/frozenblock/lib/storage/api/NoInteractionStorage.java
+++ src/main/java/net/frozenblock/lib/storage/api/NoInteractionStorage.java
@@ -23,30 +23,30 @@ import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
 import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
 import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext;
 
-public interface NoInteractionStorage<T> extends Storage<T> {
+public class NoInteractionStorage<T> extends Storage<T> {
 
     @Override
-    default long insert(T resource, long maxAmount, TransactionContext transaction) {
+    public long insert(T resource, long maxAmount, TransactionContext transaction) {
         return 0;
     }
 
     @Override
-    default long extract(T resource, long maxAmount, TransactionContext transaction) {
+    public long extract(T resource, long maxAmount, TransactionContext transaction) {
         return 0;
     }
 
     @Override
-    default boolean supportsInsertion() {
+    public boolean supportsInsertion() {
         return false;
     }
 
     @Override
-    default boolean supportsExtraction() {
+    public boolean supportsExtraction() {
         return false;
     }
 
     @Override
-    default Iterator<StorageView<T>> iterator() {
+    public Iterator<StorageView<T>> iterator() {
         return new Iterator<>() {
             @Override
             public boolean hasNext() {
