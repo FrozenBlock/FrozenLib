e42123e8fa3137d8ca0652641fc6ce0439279960
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-11-26 14:28:22 -0600

    refactor

--- src/main/kotlin/net/frozenblock/lib/core/api/StringUtil.kt
+++ src/main/kotlin/net/frozenblock/lib/core/api/StringUtil.kt
@@ -16,7 +16,7 @@
  * along with this program; if not, see <https://www.gnu.org/licenses/>.
  */
 
-package net.frozenblock.lib.util
+package net.frozenblock.lib.core.api
 
 import net.minecraft.resources.ResourceLocation
 import java.net.URI
