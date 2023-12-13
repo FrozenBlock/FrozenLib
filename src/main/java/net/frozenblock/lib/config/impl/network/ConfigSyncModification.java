39d111f3c1ef2fc55f5dd9d0488b54e83c164225
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-12-13 00:42:38 -0500

    mute error since behavior still works?

--- src/main/java/net/frozenblock/lib/config/impl/network/ConfigSyncModification.java
+++ src/main/java/net/frozenblock/lib/config/impl/network/ConfigSyncModification.java
@@ -43,7 +43,7 @@ public record ConfigSyncModification<T>(Config<T> config, DataSupplier<T> dataSu
 		try {
 			ConfigSyncData<T> syncData = dataSupplier.get(config);
 			if (syncData == null || !FrozenNetworking.connectedToServer()) {
-				new Exception("Attempted to sync config " + config.path() + " for mod " + config.modId() + " outside a server!").printStackTrace();
+				//new Exception("Attempted to sync config " + config.path() + " for mod " + config.modId() + " outside a server!").printStackTrace();
 				return;
 			}
 			T source = syncData.instance();
