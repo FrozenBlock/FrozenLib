682c59c85825b3f90bd3047ed64d42e626e4e497
Treetrain1 <treetrain1@gmail.com>
2024-01-29 17:49:14 -0600

    this gravity does NOT work

--- src/main/java/net/frozenblock/lib/gravity/mixin/LivingEntityMixin.java
+++ src/main/java/net/frozenblock/lib/gravity/mixin/LivingEntityMixin.java
@@ -18,19 +18,34 @@
 
 package net.frozenblock.lib.gravity.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.frozenblock.lib.gravity.api.GravityAPI;
+import net.frozenblock.lib.gravity.impl.EntityGravityInterface;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+public abstract class LivingEntityMixin implements EntityGravityInterface {
 
 	// TODO: convert to directional
 	@ModifyVariable(method = "travel", at = @At(value = "STORE", ordinal = 0))
 	private double useGravity(double original) {
 		LivingEntity entity = LivingEntity.class.cast(this);
 		return original * GravityAPI.calculateGravity(entity).length();
+	}
+
+	@WrapOperation(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;setDeltaMovement(DDD)V", ordinal = 3))
+	private void newGravity(LivingEntity instance, double x, double y, double z, Operation<Void> original) {
+		Vec3 gravityVec = this.frozenLib$getEffectiveGravity();
+		Vec3 directional = new Vec3(x, 0, z).add(
+			gravityVec.scale(y)
+		);
+
+		original.call(instance, directional.x, directional.y, directional.z);
 	}
 }
