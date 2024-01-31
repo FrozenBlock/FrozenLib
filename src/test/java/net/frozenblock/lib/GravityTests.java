f0161885c3ebf9c6a689ca5bb3e9075eddc16e63
Treetrain1 <treetrain1@gmail.com>
2024-01-31 16:23:27 -0600

    fix test

--- src/test/java/net/frozenblock/lib/GravityTests.java
+++ src/test/java/net/frozenblock/lib/GravityTests.java
@@ -23,6 +23,7 @@ import net.frozenblock.lib.gravity.api.GravityBelt;
 import net.frozenblock.lib.gravity.api.functions.AbsoluteGravityFunction;
 import net.minecraft.world.level.Level;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import net.minecraft.world.phys.Vec3;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -36,14 +37,14 @@ class GravityTests {
     @Test
     void testGravity() {
         // gravity 0.1 y 300..319
-        GravityAPI.register(Level.OVERWORLD, new GravityBelt<>(300.0, 319.0, new AbsoluteGravityFunction(0.1)));
+        GravityAPI.register(Level.OVERWORLD, new GravityBelt<>(300.0, 319.0, new AbsoluteGravityFunction(new Vec3(0.0, 0.1, 0.0))));
         // gravity 100.0 y -64..-32
-        GravityAPI.register(Level.OVERWORLD, new GravityBelt<>(-64.0, -32.0, new AbsoluteGravityFunction(100.0)));
+        GravityAPI.register(Level.OVERWORLD, new GravityBelt<>(-64.0, -32.0, new AbsoluteGravityFunction(new Vec3(0.0, 100.0, 0.0))));
         // gravity 0.5 y 0..15
-        GravityAPI.register(Level.OVERWORLD, new GravityBelt<>(0.0, 15.0, new AbsoluteGravityFunction(0.5)));
+        GravityAPI.register(Level.OVERWORLD, new GravityBelt<>(0.0, 15.0, new AbsoluteGravityFunction(new Vec3(0.0, 0.5, 0.0))));
 
-        assertEquals(0.1, GravityAPI.calculateGravity(Level.OVERWORLD, 300));
-        assertEquals(100.0, GravityAPI.calculateGravity(Level.OVERWORLD, -64));
-        assertEquals(0.5, GravityAPI.calculateGravity(Level.OVERWORLD, 0));
+        assertEquals(new Vec3(0.0, 0.1, 0.0), GravityAPI.calculateGravity(Level.OVERWORLD, 300));
+        assertEquals(new Vec3(0.0, 100.0, 0.0), GravityAPI.calculateGravity(Level.OVERWORLD, -64));
+        assertEquals(new Vec3(0.0, 0.5, 0.0), GravityAPI.calculateGravity(Level.OVERWORLD, 0));
     }
 }
