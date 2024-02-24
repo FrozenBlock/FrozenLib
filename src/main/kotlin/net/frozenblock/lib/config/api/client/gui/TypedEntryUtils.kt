938c4a0768e725949216a097171d45d506f84b70
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-02-23 23:13:48 -0500

    Update TypedEntryUtils.kt

--- src/main/kotlin/net/frozenblock/lib/config/api/client/gui/TypedEntryUtils.kt
+++ src/main/kotlin/net/frozenblock/lib/config/api/client/gui/TypedEntryUtils.kt
@@ -66,7 +66,7 @@ fun <T> typedEntryList(
         },
         // Save Consumer
         {
-                newValue -> setterConsumer.accept(TypedEntry(typedEntry.type, newValue))
+                newValue -> setterConsumer.accept(TypedEntry(typedEntry.type(), newValue))
         },
         // Default Value
         defaultValue.get()::value,
