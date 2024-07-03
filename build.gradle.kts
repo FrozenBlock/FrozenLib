ffe5330f709bddf9296582025d4ca05fa28d59f1
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-07-03 18:13:14 -0400

    no parchment

--- build.gradle.kts
+++ build.gradle.kts
@@ -235,7 +235,7 @@ dependencies {
 	mappings(loom.layered {
 		// please annoy treetrain if this doesn't work
         mappings("org.quiltmc:quilt-mappings:$quilt_mappings:intermediary-v2")
-        parchment("org.parchmentmc.data:parchment-$parchment_mappings@zip")
+        // parchment("org.parchmentmc.data:parchment-$parchment_mappings@zip")
 		officialMojangMappings {
 			nameSyntheticMembers = false
 		}
