e7b454cf244e8ea77925d5ac293ac38b5bd64094
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-07-26 01:22:48 -0400

    Update StructureTemplateMixin.java

--- src/main/java/net/frozenblock/lib/worldgen/structure/mixin/StructureTemplateMixin.java
+++ src/main/java/net/frozenblock/lib/worldgen/structure/mixin/StructureTemplateMixin.java
@@ -21,13 +21,11 @@ import java.util.ArrayList;
 import java.util.List;
 import net.frozenblock.lib.worldgen.structure.impl.StructureTemplateInterface;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -39,10 +37,6 @@ public class StructureTemplateMixin implements StructureTemplateInterface {
 
 	@Unique
 	private final List<StructureProcessor> frozenLib$additionalProcessors = new ArrayList<>();
-
-	@Unique
-	@Nullable
-	private ResourceLocation id;
 
 	@Inject(
 		method = "placeInWorld",
