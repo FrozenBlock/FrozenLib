c7411f726658de9fe669e3a28e82b4faa46bd772
Treetrain1 <treetrain1@gmail.com>
2024-09-30 20:22:11 -0500

    Loom 1.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ buildscript {
 }
 
 plugins {
-	id("fabric-loom") version("+")
+	id("fabric-loom") version("1.7.+")
 	id("org.ajoberstar.grgit") version("+")
 	id("org.quiltmc.gradle.licenser") version("+")
 	id("com.modrinth.minotaur") version("+")
