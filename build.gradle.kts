9f64c90d6da805c15d934e71d81099b24caaaf26
Treetrain1 <treetrain1@gmail.com>
2024-10-09 22:12:44 -0500

    Get xjs-data from Jitpack + update to 0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -268,7 +268,7 @@ dependencies {
 
     // ExJson
     //relocModApi("org.exjson:xjs-data:$xjs_data_version")
-    relocModApi(files("libs/xjs-data-0.5.jar"))
+    relocModApi("com.github.Treetrain1:xjs-data:infinity-compat-SNAPSHOT")
     relocModApi("org.exjson:xjs-compat:$xjs_compat_version")
     relocModApi("com.personthecat:fresult:$fresult_version")
     compileOnly("org.projectlombok:lombok:1.18.34")?.let { annotationProcessor(it) }
