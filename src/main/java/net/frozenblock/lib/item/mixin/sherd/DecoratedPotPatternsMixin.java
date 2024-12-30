a76e102e26e379f7baade0a6ac23e6179f9070b6
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-12-30 15:03:21 -0500

    why doesnt this work

--- src/main/java/net/frozenblock/lib/item/mixin/sherd/DecoratedPotPatternsMixin.java
+++ src/main/java/net/frozenblock/lib/item/mixin/sherd/DecoratedPotPatternsMixin.java
@@ -17,27 +17,52 @@
 
 package net.frozenblock.lib.item.mixin.sherd;
 
+import com.google.common.collect.ImmutableMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.fabricmc.loader.api.FabricLoader;
 import net.frozenblock.lib.item.impl.sherd.DecoratedPotPatternRegistryEntrypoint;
 import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.entity.DecoratedPotPattern;
 import net.minecraft.world.level.block.entity.DecoratedPotPatterns;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import java.util.Map;
 
-@Mixin(DecoratedPotPatterns.class)
+@Mixin(value = DecoratedPotPatterns.class, priority = 50)
 public class DecoratedPotPatternsMixin {
 
-	@Inject(method = "bootstrap", at = @At(value = "RETURN", shift = At.Shift.BEFORE))
+	@Shadow
+	public static Map<Item, ResourceKey<DecoratedPotPattern>> ITEM_TO_POT_TEXTURE;
+
+	@Inject(method = "<clinit>", at = @At(value = "TAIL"))
+	private static void frozenLib$createMap(CallbackInfo info) {
+		Map<Item, ResourceKey<DecoratedPotPattern>> mutableSherdMap = new Object2ObjectOpenHashMap<>(ITEM_TO_POT_TEXTURE);
+		FabricLoader.getInstance().getEntrypointContainers("frozenlib:decorated_pot_patterns", DecoratedPotPatternRegistryEntrypoint.class).forEach(entrypoint -> {
+			DecoratedPotPatternRegistryEntrypoint decoratedPotPatternRegistryEntrypoint = entrypoint.getEntrypoint();
+			decoratedPotPatternRegistryEntrypoint.registerForItems(mutableSherdMap);
+		});
+		ITEM_TO_POT_TEXTURE = ImmutableMap.copyOf(mutableSherdMap);
+	}
+
+	@Inject(
+		method = "bootstrap",
+		at = @At(
+			value = "RETURN",
+			shift = At.Shift.BEFORE
+		)
+	)
 	private static void frozenLib$bootstrap(Registry<DecoratedPotPattern> registry, CallbackInfoReturnable<DecoratedPotPattern> info) {
 		FabricLoader.getInstance().getEntrypointContainers("frozenlib:decorated_pot_patterns", DecoratedPotPatternRegistryEntrypoint.class).forEach(entrypoint -> {
 			try {
 				DecoratedPotPatternRegistryEntrypoint decoratedPotPatternRegistryEntrypoint = entrypoint.getEntrypoint();
 				decoratedPotPatternRegistryEntrypoint.bootstrap(registry);
-			} catch (Throwable ignored) {
-			}
+			} catch (Throwable ignored) {}
 		});
 	}
 }
