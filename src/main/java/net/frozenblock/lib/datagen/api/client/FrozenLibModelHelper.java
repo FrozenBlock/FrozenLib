5731d5093bc5069add2b54b0459047d72b02ae21
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-02-16 00:44:23 -0500

    no uv lock gen

--- src/main/java/net/frozenblock/lib/datagen/api/client/FrozenLibModelHelper.java
+++ src/main/java/net/frozenblock/lib/datagen/api/client/FrozenLibModelHelper.java
@@ -17,9 +17,12 @@
 
 package net.frozenblock.lib.datagen.api.client;
 
+import com.google.common.collect.ImmutableMap;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 import net.frozenblock.lib.FrozenLibConstants;
 import net.minecraft.client.color.item.GrassColorSource;
 import net.minecraft.client.data.models.BlockModelGenerators;
@@ -29,15 +32,27 @@ import net.minecraft.client.data.models.model.TextureMapping;
 import net.minecraft.client.data.models.model.TextureSlot;
 import net.minecraft.client.data.models.model.TexturedModel;
 import net.minecraft.client.renderer.block.model.MultiVariant;
+import net.minecraft.client.renderer.block.model.VariantMutator;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.StateHolder;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.Property;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
 
 @Environment(EnvType.CLIENT)
 public class FrozenLibModelHelper {
+	public static final Map<Direction, VariantMutator> MULTIFACE_GENERATOR_NO_UV_LOCK = ImmutableMap.of(
+		Direction.NORTH, BlockModelGenerators.NOP,
+		Direction.EAST, BlockModelGenerators.Y_ROT_90,
+		Direction.SOUTH, BlockModelGenerators.Y_ROT_180,
+		Direction.WEST, BlockModelGenerators.Y_ROT_270,
+		Direction.UP, BlockModelGenerators.X_ROT_270,
+		Direction.DOWN, BlockModelGenerators.X_ROT_90
+	);
 	public static final TexturedModel.Provider TINTED_FLOWERBED_1 = TexturedModel.createDefault(
 		TextureMapping::flowerbed,
 		createModelTemplate("tinted_flowerbed_1", "_1", TextureSlot.FLOWERBED, TextureSlot.STEM)
@@ -131,6 +146,20 @@ public class FrozenLibModelHelper {
 						multiVariant4.with(BlockModelGenerators.Y_ROT_270)
 					)
 			);
+	}
+
+	public static <T extends Property<?>> @Unmodifiable @NotNull Map<T, VariantMutator> selectMultifaceNoUvLockProperties(
+		StateHolder<?, ?> stateHolder,
+		Function<Direction, T> function
+	) {
+		ImmutableMap.Builder<T, VariantMutator> builder = ImmutableMap.builderWithExpectedSize(MULTIFACE_GENERATOR_NO_UV_LOCK.size());
+		MULTIFACE_GENERATOR_NO_UV_LOCK.forEach((direction, variantMutator) -> {
+			T property = function.apply(direction);
+			if (stateHolder.hasProperty(property)) {
+				builder.put(property, variantMutator);
+			}
+		});
+		return builder.build();
 	}
 
 	@Contract("_, _, _ -> new")
