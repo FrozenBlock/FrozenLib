f12401f8bca80e7fdd169ee66cff586d2383d359
AViewFromTheTop <AViewFromTheTop@users.noreply.github.com>
2025-02-28 02:39:01 +0000

    Apply automatic changes

--- src/main/java/net/frozenblock/lib/worldgen/feature/api/feature/noise_path/NoisePathFeature.java
+++ src/main/java/net/frozenblock/lib/worldgen/feature/api/feature/noise_path/NoisePathFeature.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (C) 2024-2025 FrozenBlock
+ * Copyright (C) 2025 FrozenBlock
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published startY
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
