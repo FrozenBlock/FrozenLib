4bd8a1e7ab59e87125ecc943089c5a8b7e3d57b6
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-05-24 13:20:21 -0400

    optimize & use tags so this can be used with api

--- src/main/java/net/frozenblock/lib/worldgen/structure/api/BlockStateRespectingBiomeProcessorRule.java
+++ src/main/java/net/frozenblock/lib/worldgen/structure/api/BlockStateRespectingBiomeProcessorRule.java
@@ -21,11 +21,10 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
-import net.minecraft.core.HolderSet;
-import net.minecraft.core.RegistryCodecs;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
@@ -43,7 +42,7 @@ public class BlockStateRespectingBiomeProcessorRule {
 	public static final Passthrough DEFAULT_BLOCK_ENTITY_MODIFIER = Passthrough.INSTANCE;
 	public static final Codec<BlockStateRespectingBiomeProcessorRule> CODEC = RecordCodecBuilder.create(
 		instance -> instance.group(
-			RegistryCodecs.homogeneousList(Registries.BIOME).fieldOf("biomes").forGetter(rule -> rule.biomes),
+			TagKey.codec(Registries.BIOME).fieldOf("biomes").forGetter(rule -> rule.biomes),
 			RuleTest.CODEC.fieldOf("input_predicate").forGetter(rule -> rule.inputPredicate),
 			RuleTest.CODEC.fieldOf("location_predicate").forGetter(rule -> rule.locPredicate),
 			PosRuleTest.CODEC.lenientOptionalFieldOf("position_predicate", PosAlwaysTrueTest.INSTANCE).forGetter(rule -> rule.posPredicate),
@@ -51,23 +50,23 @@ public class BlockStateRespectingBiomeProcessorRule {
 			RuleBlockEntityModifier.CODEC.lenientOptionalFieldOf("block_entity_modifier", DEFAULT_BLOCK_ENTITY_MODIFIER).forGetter(rule -> rule.blockEntityModifier)
 		).apply(instance, BlockStateRespectingBiomeProcessorRule::new)
 	);
-	private final HolderSet<Biome> biomes;
+	private final TagKey<Biome> biomes;
 	private final RuleTest inputPredicate;
 	private final RuleTest locPredicate;
 	private final PosRuleTest posPredicate;
 	private final Block outputBlock;
 	private final RuleBlockEntityModifier blockEntityModifier;
 
-	public BlockStateRespectingBiomeProcessorRule(HolderSet<Biome> biomes, RuleTest inputPredicate, RuleTest locationPredicate, Block outputBlock) {
+	public BlockStateRespectingBiomeProcessorRule(TagKey<Biome> biomes, RuleTest inputPredicate, RuleTest locationPredicate, Block outputBlock) {
 		this(biomes, inputPredicate, locationPredicate, PosAlwaysTrueTest.INSTANCE, outputBlock);
 	}
 
-	public BlockStateRespectingBiomeProcessorRule(HolderSet<Biome> biomes, RuleTest inputPredicate, RuleTest locationPredicate, PosRuleTest positionPredicate, Block outputBlock) {
+	public BlockStateRespectingBiomeProcessorRule(TagKey<Biome> biomes, RuleTest inputPredicate, RuleTest locationPredicate, PosRuleTest positionPredicate, Block outputBlock) {
 		this(biomes, inputPredicate, locationPredicate, positionPredicate, outputBlock, DEFAULT_BLOCK_ENTITY_MODIFIER);
 	}
 
 	public BlockStateRespectingBiomeProcessorRule(
-		HolderSet<Biome> biomes, RuleTest inputPredicate, RuleTest locationPredicate, PosRuleTest positionPredicate, Block outputBlock, RuleBlockEntityModifier ruleBlockEntityModifier
+		TagKey<Biome> biomes, RuleTest inputPredicate, RuleTest locationPredicate, PosRuleTest positionPredicate, Block outputBlock, RuleBlockEntityModifier ruleBlockEntityModifier
 	) {
 		this.biomes = biomes;
 		this.inputPredicate = inputPredicate;
@@ -77,11 +76,12 @@ public class BlockStateRespectingBiomeProcessorRule {
 		this.blockEntityModifier = ruleBlockEntityModifier;
 	}
 
-	public boolean test(Holder<Biome> biome, BlockState input, BlockState location, BlockPos localPos, BlockPos absolutePos, BlockPos pivot, RandomSource random) {
-		return this.biomes.contains(biome)
-			&& this.inputPredicate.test(input, random)
-			&& this.locPredicate.test(location, random)
-			&& this.posPredicate.test(localPos, absolutePos, pivot, random);
+	public boolean test(BlockState input, BlockState location, BlockPos localPos, BlockPos absolutePos, BlockPos pivot, RandomSource random) {
+		return this.inputPredicate.test(input, random) && this.locPredicate.test(location, random) && this.posPredicate.test(localPos, absolutePos, pivot, random);
+	}
+
+	public boolean doesBiomeMatch(Holder<Biome> biome) {
+		return biome.is(this.biomes);
 	}
 
 	public BlockState getOutputState(BlockState inputState) {
