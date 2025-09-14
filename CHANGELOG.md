18136e76f5adfa16aead3b8b434384f76ffe1757
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-09-13 23:39:08 -0400

    Add CustomRotationalParticleRenderer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,24 +4,27 @@ Put the changelog BELOW the dashes. ANYTHING ABOVE IS IGNORED.
 - Fixed an issue with server textures to work as intended.
 - Added `MarkForPostProcessingProcessor,` a structure processor that will mark matching blocks for post-processing.
 - Added `DataMarkerProcessableLegacySinglePoolElement` and `DataMarkerProcessableSinglePoolElement,` jigsaw structure pool elements that do not remove Structure Blocks upon being placed.
-  - Data Blocks can be processed by writing a mixin on `StructurePoolElement`'s `handleDataMarker` method.
+    - Data Blocks can be processed by writing a mixin on `StructurePoolElement`'s `handleDataMarker` method.
 - Fixed an issue that prevented Breezes from emitting a wind disturbance.
 - Fixed an issue that allowed structures disabled via the `StructureGenerationConditionApi` to still cause lag when using the locate command.
 - Added the `downloadResourcePack` and `downloadResourcePacks` methods to `FrozenLibModResourcePackApi`!
-  - This method will download a resource pack/resource packs, which will be loaded either open Minecraft's next boot or resources being reloaded.
-  - Resource Pack downloads can be "grouped" together using the `PackDownloadGroup` class.
-  - The file the URL points to must be a JSON with the two following fields:
-    - `pack`: The URL of the resource pack, which must be a `.zip` file.
-    - `version`: A numerical id for the resource pack. If the pack has been downloaded before with the same `version`, the download will be skipped.
-  - A toast will be displayed on-screen to tell the player when a pack has finished or failed downloading.
-  - A config option to disable pack downloading and toasts has been added.
-- Added `FrozenLibConfiguredTreeFeature` and `FrozenLibPlacedTreeFeature,` designed to speed up the process of designing tree variants both with and without leaf litters.
+    - This method will download a resource pack/resource packs, which will be loaded either open Minecraft's next boot or resources being reloaded.
+    - Resource Pack downloads can be "grouped" together using the `PackDownloadGroup` class.
+    - The file the URL points to must be a JSON with the two following fields:
+        - `pack`: The URL of the resource pack, which must be a `.zip` file.
+        - `version`: A numerical id for the resource pack. If the pack has been downloaded before with the same `version`, the download will be skipped.
+    - A toast will be displayed on-screen to tell the player when a pack has finished or failed downloading.
+    - A config option to disable pack downloading and toasts has been added.
+- Added the `CustomRotationalParticleRenderer` class, used to make the creation of particles with 3D rotation much easier.
+  - An example of this can be seen in the `WindParticle` class.
+- Removed two unused item textures.
 
 ### 1.21.5+
-- All Block textures with the `_frozenlib_emissive` suffix appended to their name will now be applied to models as an emissive overlay!
-- Added the `frozenlib_emissive` metadata section for texture `mcmeta` files, allowing you to specify the emissive overlay's light emission, and whether it should have block shading.
-- Block Render Types can now be overriden, letting players and modders alike specify how they want a Block to render!
-  - This is achieved via a new `block_render_type_overwrites` path within Resource Packs.
-  - Each `json` file must be named after its respective block.
-  - Each `json` file must contain the new Render Type in the `render_type` field.
-    - This can be `solid`, `cutout_mipped`, `cutout`, `translucent`, or `tripwire`.
+- Added `FrozenLibConfiguredTreeFeature` and `FrozenLibPlacedTreeFeature,` designed to speed up the process of designing tree variants both with and without leaf litters.
+
+### 1.21.9+
+- Removed FrozenLib's modified Vanilla debug renderers.
+- Removed FrozenLib's modified Vanilla debug packets.
+- Removed FrozenLib's `DebugRendererEvents` class.
+- Removed FrozenLIb's custom Debug menu.
+- FrozenLib's Wind and Wind Disturbance debug renderers are now enabled via the `MC_DEBUG_FROZENLIB_WIND` and `MC_DEBUG_FROZENLIB_WIND_DISTURBANCE` launch arguments.
---
+++ src/main/java/net/frozenblock/lib/particle/client/CustomRotationalParticleRenderer.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (C) 2024-2025 FrozenBlock
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.frozenblock.lib.particle.client;
+
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import java.util.function.Consumer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.frozenblock.lib.math.api.AdvancedMath;
+import net.minecraft.client.Camera;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
+
+@Environment(EnvType.CLIENT)
+public class CustomRotationalParticleRenderer {
+	private static final Vector3f NORMALIZED_QUAT_VECTOR = new Vector3f(0.5F, 0.5F, 0.5F).normalize();
+
+	private float prevXRot;
+	private float xRot;
+	private float prevYRot;
+	private float yRot;
+
+	private float prevRotMultiplier;
+	private float rotMultiplier;
+
+	private boolean flipped;
+
+	public CustomRotationalParticleRenderer() {
+	}
+
+	public void setPrevRotationFromCurrent() {
+		this.prevXRot = this.xRot;
+		this.prevYRot = this.yRot;
+	}
+
+	public void setRotation(float xRot, float yRot) {
+		this.yRot = yRot;
+		this.xRot = xRot;
+		this.setRotMultiplierFromXRot();
+	}
+
+	public void setRotationFromMovement(double xd, double yd, double zd, float rotationAmount) {
+		final double horizontalDistance = Math.sqrt((xd * xd) + (zd * zd));
+		double newYRot = (Mth.atan2(xd, zd)) * Mth.RAD_TO_DEG;
+		double newXRot = (Mth.atan2(horizontalDistance, yd)) * Mth.RAD_TO_DEG;
+
+		if (Math.abs(newYRot - this.prevYRot) > 180D) newYRot += 360D;
+		if (Math.abs(newXRot - this.prevXRot) > 180D) newXRot += 360D;
+
+		double newYRotDifference = newYRot - this.yRot;
+		double newXRotDifference = newXRot - this.xRot;
+
+		this.yRot += (float)newYRotDifference * rotationAmount;
+		this.xRot += (float)newXRotDifference * rotationAmount;
+
+		if (this.yRot > 360F) {
+			this.yRot -= 360F;
+			this.prevYRot -= 360F;
+		} else if (this.yRot < 0F) {
+			this.yRot += 360F;
+			this.prevYRot += 360F;
+		}
+
+		if (this.xRot > 360F) {
+			this.xRot -= 360F;
+			this.prevXRot -= 360F;
+		} else if (this.xRot < 0F) {
+			this.xRot += 360F;
+			this.prevXRot += 360F;
+		}
+
+		this.setRotMultiplierFromXRot();
+	}
+
+	private void setRotMultiplierFromXRot() {
+		this.prevRotMultiplier = this.rotMultiplier;
+		this.rotMultiplier = Mth.sin((this.xRot * Mth.PI) / 180F);
+	}
+
+	public void setFlipped(boolean flipped) {
+		this.flipped = flipped;
+	}
+
+	private static float [] getUVMapping(float U0, float U1, float V0, float V1, boolean flipped) {
+		return new float[]{
+			flipped ? U1 : U0,
+			flipped ? U0 : U1,
+			V0,
+			V1
+		};
+	}
+
+	public void render(
+		@NotNull VertexConsumer buffer,
+		@NotNull Camera camera,
+		float x, float y, float z,
+		double xd, double yd, double zd,
+		float U0, float U1, float V0, float V1,
+		float quadSize,
+		float rCol, float gCol, float bCol, float alpha,
+		int light,
+		float partialTicks
+	) {
+		final float yRot = Mth.lerp(partialTicks, this.prevYRot, this.yRot) * Mth.DEG_TO_RAD;
+		final float xRot = Mth.lerp(partialTicks, this.prevXRot, this.xRot) * -Mth.DEG_TO_RAD;
+		final float cameraRotWhileVertical = ((-camera.getYRot()) * (1F - Mth.lerp(partialTicks, this.prevRotMultiplier, this.rotMultiplier))) * Mth.DEG_TO_RAD;
+		float cameraXRot = camera.getXRot();
+		float rotationOffset = 90F;
+
+		Vec3 particlePos = new Vec3(x, y, z);
+		Vec3 cameraPos = camera.getPosition();
+		double normalizedRelativeY = particlePos.subtract(cameraPos).normalize().subtract(new Vec3(xd, yd, zd).normalize()).normalize().y;
+
+		double particleRotation = AdvancedMath.getAngleFromOriginXZ(new Vec3(xd, 0D, zd));
+		double angleToParticle = AdvancedMath.getAngleBetweenXZ(particlePos, cameraPos);
+		double relativeParticleAngle = (360D + (angleToParticle - particleRotation)) % 360D;
+
+		if (normalizedRelativeY > 0D) {
+			cameraXRot = Mth.lerp(Mth.square((float)normalizedRelativeY), cameraXRot, -90F);
+		} else if (normalizedRelativeY < 0D) {
+			cameraXRot = Mth.lerp(Mth.square((float)Math.abs(normalizedRelativeY)), cameraXRot, 90F);
+		}
+
+		float fixedRotation = rotationOffset + cameraXRot;
+		if (relativeParticleAngle > 0D && relativeParticleAngle < 180D) fixedRotation *= -1F;
+
+		float cameraRotWhileSideways = ((fixedRotation) * (Mth.lerp(partialTicks, this.prevRotMultiplier, this.rotMultiplier))) * Mth.DEG_TO_RAD;
+		this.renderParticle(
+			buffer,
+			cameraPos,
+			x, y, z,
+			quadSize,
+			rCol, gCol, bCol, alpha,
+			light,
+			transforms -> transforms.rotateY(yRot)
+				.rotateX(-xRot)
+				.rotateY(cameraRotWhileSideways)
+				.rotateY(cameraRotWhileVertical),
+			getUVMapping(U0, U1, V0, V1, this.flipped)
+		);
+		this.renderParticle(
+			buffer,
+			cameraPos,
+			x, y, z,
+			quadSize,
+			rCol, gCol, bCol, alpha,
+			light,
+			transforms -> transforms.rotateY((float) -Math.PI + yRot)
+				.rotateX(xRot)
+				.rotateY(cameraRotWhileSideways)
+				.rotateY(cameraRotWhileVertical),
+			getUVMapping(U0, U1, V0, V1, !this.flipped)
+		);
+	}
+
+	private void renderParticle(
+		VertexConsumer buffer,
+		@NotNull Vec3 cameraPos,
+		float x, float y, float z,
+		float quadSize,
+		float rCol, float gCol, float bCol, float alpha,
+		int light,
+		@NotNull Consumer<Quaternionf> quaternionConsumer,
+		float[] uvMapping
+	) {
+		float xDifference = (float)(x - cameraPos.x());
+		float yDifference = (float)(y - cameraPos.y());
+		float zDifference = (float)(z - cameraPos.z());
+		Quaternionf quaternionf = new Quaternionf().setAngleAxis(0F, NORMALIZED_QUAT_VECTOR.x(), NORMALIZED_QUAT_VECTOR.y(), NORMALIZED_QUAT_VECTOR.z());
+		quaternionConsumer.accept(quaternionf);
+		Vector3f[] vector3fs = new Vector3f[]{
+			new Vector3f(-1F, -1F, 0F), new Vector3f(-1F, 1F, 0F), new Vector3f(1F, 1F, 0F), new Vector3f(1F, -1F, 0F)
+		};
+
+		for(int j = 0; j < 4; ++j) {
+			Vector3f vector3f2 = vector3fs[j];
+			vector3f2.rotate(quaternionf);
+			vector3f2.mul(quadSize);
+			vector3f2.add(xDifference, yDifference, zDifference);
+		}
+
+		buffer.addVertex(vector3fs[0].x(), vector3fs[0].y(), vector3fs[0].z())
+			.setUv(uvMapping[1], uvMapping[3])
+			.setColor(rCol, gCol, bCol, alpha)
+			.setLight(light);
+		buffer.addVertex(vector3fs[1].x(), vector3fs[1].y(), vector3fs[1].z())
+			.setUv(uvMapping[1], uvMapping[2])
+			.setColor(rCol, gCol, bCol, alpha)
+			.setLight(light);
+		buffer.addVertex(vector3fs[2].x(), vector3fs[2].y(), vector3fs[2].z())
+			.setUv(uvMapping[0], uvMapping[2])
+			.setColor(rCol, gCol, bCol, alpha)
+			.setLight(light);
+		buffer.addVertex(vector3fs[3].x(), vector3fs[3].y(), vector3fs[3].z())
+			.setUv(uvMapping[0], uvMapping[3])
+			.setColor(rCol, gCol, bCol, alpha)
+			.setLight(light);
+	}
+
+}
