9450ad53107930d7fa10fca9c87e33c5039baee7
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-12-24 13:27:26 -0500

    idfk what im doing so i stole other stuff

--- src/main/java/net/frozenblock/lib/config/newconfig/gui/TestConfigScreen.java
+++ src/main/java/net/frozenblock/lib/config/newconfig/gui/TestConfigScreen.java
@@ -17,16 +17,106 @@
 
 package net.frozenblock.lib.config.newconfig.gui;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Options;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.options.OptionsSubScreen;
+import net.minecraft.client.gui.screens.options.controls.KeyBindsList;
+import net.minecraft.client.input.KeyEvent;
+import net.minecraft.client.input.MouseButtonEvent;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.Util;
+import org.jspecify.annotations.Nullable;
 
 @Environment(EnvType.CLIENT)
-public class TestConfigScreen extends Screen {
+public class TestConfigScreen extends OptionsSubScreen {
+	private static final Component TITLE = Component.translatable("controls.keybinds.title");
+	@Nullable
+	public KeyMapping selectedKey;
+	public long lastKeySelection;
+	private ConfigEntriesList configEntriesList;
+	private Button resetButton;
 
-	protected TestConfigScreen(Component title) {
-		super(title);
+	public TestConfigScreen(final Screen lastScreen, final Options options) {
+		super(lastScreen, options, TITLE);
 	}
 
+	@Override
+	protected void addContents() {
+		this.configEntriesList = this.layout.addToContents(new ConfigEntriesList(this, this.minecraft));
+	}
+
+	@Override
+	protected void addOptions() {
+	}
+
+	@Override
+	protected void addFooter() {
+		this.resetButton = Button.builder(Component.translatable("controls.resetAll"), button -> {
+			for (KeyMapping key : this.options.keyMappings) key.setKey(key.getDefaultKey());
+
+			this.configEntriesList.resetMappingAndUpdateButtons();
+		}).build();
+		LinearLayout bottomButtons = this.layout.addToFooter(LinearLayout.horizontal().spacing(8));
+		bottomButtons.addChild(this.resetButton);
+		bottomButtons.addChild(Button.builder(CommonComponents.GUI_DONE, button -> this.onClose()).build());
+	}
+
+	@Override
+	protected void repositionElements() {
+		this.layout.arrangeElements();
+		this.configEntriesList.updateSize(this.width, this.layout);
+	}
+
+	@Override
+	public boolean mouseClicked(final MouseButtonEvent event, final boolean doubleClick) {
+		if (this.selectedKey != null) {
+			this.selectedKey.setKey(InputConstants.Type.MOUSE.getOrCreate(event.button()));
+			this.selectedKey = null;
+			this.configEntriesList.resetMappingAndUpdateButtons();
+			return true;
+		} else {
+			return super.mouseClicked(event, doubleClick);
+		}
+	}
+
+	@Override
+	public boolean keyPressed(final KeyEvent event) {
+		if (this.selectedKey != null) {
+			if (event.isEscape()) {
+				this.selectedKey.setKey(InputConstants.UNKNOWN);
+			} else {
+				this.selectedKey.setKey(InputConstants.getKey(event));
+			}
+
+			this.selectedKey = null;
+			this.lastKeySelection = Util.getMillis();
+			this.configEntriesList.resetMappingAndUpdateButtons();
+			return true;
+		} else {
+			return super.keyPressed(event);
+		}
+	}
+
+	@Override
+	public void render(final GuiGraphics graphics, final int mouseX, final int mouseY, final float a) {
+		super.render(graphics, mouseX, mouseY, a);
+		boolean canReset = false;
+
+		for (KeyMapping key : this.options.keyMappings) {
+			if (!key.isDefault()) {
+				canReset = true;
+				break;
+			}
+		}
+
+		this.resetButton.active = canReset;
+	}
 }
---
+++ src/main/java/net/frozenblock/lib/config/newconfig/gui/entry/BooleanEntry.java
@@ -0,0 +1,8 @@
+package net.frozenblock.lib.config.newconfig.gui.entry;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class BooleanEntry {
+}
