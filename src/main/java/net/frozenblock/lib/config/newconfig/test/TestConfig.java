88b3447c942b545baabe7fe8dcaf2fa111f77d6d
Ethan Stokes <erstokes10@gmail.com>
2025-12-25 21:19:51 -0600

    add poopcrap test

--- src/main/java/net/frozenblock/lib/config/newconfig/test/TestConfig.java
+++ src/main/java/net/frozenblock/lib/config/newconfig/test/TestConfig.java
@@ -17,6 +17,8 @@
 
 package net.frozenblock.lib.config.newconfig.test;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.frozenblock.lib.FrozenLibConstants;
 import net.frozenblock.lib.config.newconfig.entry.ConfigEntry;
@@ -25,15 +27,37 @@ import net.frozenblock.lib.config.newconfig.instance.ConfigSettings;
 import net.frozenblock.lib.config.newconfig.ConfigSerializer;
 import net.frozenblock.lib.registry.FrozenLibRegistries;
 import net.minecraft.core.Registry;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.Identifier;
 import java.util.Optional;
 
 public class TestConfig {
+	public record PoopCrap(int poop, String crap) {
+		public static final Codec<PoopCrap> CODEC = RecordCodecBuilder.create(instance ->
+			instance.group(
+				Codec.INT.fieldOf("poop").forGetter(PoopCrap::poop),
+				Codec.STRING.fieldOf("crap").forGetter(PoopCrap::crap)
+			).apply(instance, PoopCrap::new)
+		);
+
+		public static final StreamCodec<FriendlyByteBuf, PoopCrap> STREAM_CODEC = StreamCodec.of(
+			(buf, poopcrap) -> {
+				buf.writeInt(poopcrap.poop());
+				buf.writeUtf(poopcrap.crap());
+			},
+			(buf) -> new PoopCrap(buf.readInt(), buf.readUtf())
+		);
+	}
+	public static final EntryType<PoopCrap> POOPCRAP_TYPE = EntryType.create(PoopCrap.CODEC, PoopCrap.STREAM_CODEC);
+
 	public static final ConfigEntry<Boolean> TEST = new ConfigEntry<>(id("test"), EntryType.BOOL, true);
 	public static final ConfigEntry<Identifier> TEST_ID = new ConfigEntry<>(id("test_id"), EntryType.IDENTIFIER, FrozenLibConstants.id("test_id"));
 
 	public static final ConfigEntry<Boolean> TEST1_EMBEDDED = new ConfigEntry<>(id("embedded/test1"), EntryType.BOOL, true, Optional.of("whoa awesome"));
 	public static final ConfigEntry<Integer> TEST2_EMBEDDED = new ConfigEntry<>(id("embedded/test2"), EntryType.INT, 67);
+
+	public static final ConfigEntry<PoopCrap> POOPCRAP = new ConfigEntry<>(id("poopcrap"), POOPCRAP_TYPE, new PoopCrap(67, "41"));
 
 	public static void init() {
 
