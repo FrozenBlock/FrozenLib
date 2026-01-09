a7e2b7b33a82de5d22f64a6291fd77f70cc228df
EthanRStokes <EthanRStokes@users.noreply.github.com>
2026-01-09 03:09:06 +0000

    Apply automatic changes

--- src/main/java/org/quiltmc/qsl/frozenblock/core/registry/impl/event/RegistryMonitorImpl.java
+++ src/main/java/org/quiltmc/qsl/frozenblock/core/registry/impl/event/RegistryMonitorImpl.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2024-2025 The Quilt Project
- * Copyright 2024-2025 FrozenBlock
+ * Copyright 2024-2026 The Quilt Project
+ * Copyright 2024-2026 FrozenBlock
  * Modified to work on Fabric
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
