6d8647731f68c7be834ba0e5f93b125c3a2e84f7
EthanRStokes <EthanRStokes@users.noreply.github.com>
2026-01-28 03:58:35 +0000

    Apply automatic changes

--- src/testmod/java/net/frozenblock/lib/testmod/config/gui/TestConfigGui.kt
+++ src/testmod/java/net/frozenblock/lib/testmod/config/gui/TestConfigGui.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024-2026 FrozenBlock
+ * Copyright (C) 2026 FrozenBlock
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package net.frozenblock.lib.testmod.config.gui
 
 import me.shedaniel.clothconfig2.api.ConfigBuilder
