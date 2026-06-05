61fd2e808c788cd92c2c38cd1204ea60ffddb91a
AViewFromTheTop <AViewFromTheTop@users.noreply.github.com>
2026-06-05 16:34:36 +0000

    Apply automatic changes

--- src/main/java/net/frozenblock/lib/entity/client/api/renderer/entity/AbstractBlockLikeMobRenderer.java
+++ src/main/java/net/frozenblock/lib/entity/client/api/renderer/entity/AbstractBlockLikeMobRenderer.java
@@ -1,18 +1,18 @@
 /*
- * Copyright 2025-2026 FrozenBlock
- * This file is part of Wilder Wild.
+ * Copyright (C) 2026 FrozenBlock
  *
- * This program is free software; you can modify it under
- * the terms of version 1 of the FrozenBlock Modding Oasis License
- * as published by FrozenBlock Modding Oasis.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * FrozenBlock Modding Oasis License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the FrozenBlock Modding Oasis License
- * along with this program; if not, see <https://github.com/FrozenBlock/Licenses>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package net.frozenblock.lib.entity.client.api.renderer.entity;
