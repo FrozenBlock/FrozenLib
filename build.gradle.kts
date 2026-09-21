9a9770d05ac547ed0ce17f6d517199ae5e944508
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2026-09-21 00:28:10 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -132,6 +132,10 @@ subprojects {
         }
     }
 
+    mod {
+        versionStrategy = FrozenBlockVersionStrategy()
+    }
+
     tasks.withType<JavaCompile> {
         options.compilerArgs.addAll(listOf("-Xmaxerrs", "4000"))
         options.release.set(25)
